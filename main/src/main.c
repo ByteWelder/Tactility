73ed5a5ebefd744b1718acabceb5baf000132b07
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-01-05 20:53:14 +0100

    added comment about hardware

--- main/src/main.c
+++ main/src/main.c
@@ -6,6 +6,10 @@
 
 __attribute__((unused)) void app_main(void) {
     static const Config config = {
+        /**
+         * Auto-select a board based on the ./sdkconfig.board.* file
+         * that you copied to ./sdkconfig before you opened this project.
+         */
         .hardware = NB_BOARD_HARDWARE,
         .apps = {
             &hello_world_app
