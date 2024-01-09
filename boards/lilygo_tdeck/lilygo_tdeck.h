d3e5c609f394a56dde403c94e9e690a8285735f0
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-01-09 21:18:25 +0100

    allow hardwareconfig customizations

--- boards/lilygo_tdeck/lilygo_tdeck.h
+++ boards/lilygo_tdeck/lilygo_tdeck.h
@@ -2,4 +2,17 @@
 
 #include "tactility.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Available for HardwareConfig customizations
+void lilygo_tdeck_bootstrap();
+DisplayDriver lilygo_tdeck_display_driver();
+TouchDriver lilygo_tdeck_touch_driver();
+
 extern const HardwareConfig lilygo_tdeck;
+
+#ifdef __cplusplus
+}
+#endif
--- boards/yellow_board/yellow_board.h
+++ boards/yellow_board/yellow_board.h
@@ -2,5 +2,17 @@
 
 #include "tactility.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Available for HardwareConfig customizations
+DisplayDriver board_2432s024_create_display_driver();
+TouchDriver board_2432s024_create_touch_driver();
+
 // Capacitive touch version of the 2.4" yellow board
 extern const HardwareConfig yellow_board_24inch_cap;
+
+#ifdef __cplusplus
+}
+#endif
