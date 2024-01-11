817e6a4c49741ecba39861aeb25bba8ae8925b7a
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-01-11 22:56:52 +0100

    cleanup

--- components/tactility/src/apps/system/desktop/desktop.c
+++ components/tactility/src/apps/system/desktop/desktop.c
@@ -3,7 +3,7 @@
 #include "lvgl.h"
 #include "services/loader/loader.h"
 
-static void on_open_app(lv_event_t* e) {
+static void on_app_pressed(lv_event_t* e) {
     lv_event_code_t code = lv_event_get_code(e);
     if (code == LV_EVENT_CLICKED) {
         const AppManifest* manifest = lv_event_get_user_data(e);
@@ -11,11 +11,11 @@ static void on_open_app(lv_event_t* e) {
     }
 }
 
-static void add_app_to_list(const AppManifest* manifest, void* _Nullable parent) {
+static void create_app_widget(const AppManifest* manifest, void* _Nullable parent) {
     furi_check(parent);
     lv_obj_t* list = (lv_obj_t*)parent;
     lv_obj_t* btn = lv_list_add_btn(list, LV_SYMBOL_FILE, manifest->name);
-    lv_obj_add_event_cb(btn, &on_open_app, LV_EVENT_CLICKED, (void*)manifest);
+    lv_obj_add_event_cb(btn, &on_app_pressed, LV_EVENT_CLICKED, (void*)manifest);
 }
 
 static void desktop_show(Context* context, lv_obj_t* parent) {
@@ -26,9 +26,9 @@ static void desktop_show(Context* context, lv_obj_t* parent) {
     lv_obj_center(list);
 
     lv_list_add_text(list, "System");
-    app_manifest_registry_for_each_of_type(AppTypeSystem, list, add_app_to_list);
+    app_manifest_registry_for_each_of_type(AppTypeSystem, list, create_app_widget);
     lv_list_add_text(list, "User");
-    app_manifest_registry_for_each_of_type(AppTypeUser, list, add_app_to_list);
+    app_manifest_registry_for_each_of_type(AppTypeUser, list, create_app_widget);
 }
 
 const AppManifest desktop_app = {
--- components/tactility/src/services/gui/gui_draw.c
+++ components/tactility/src/services/gui/gui_draw.c
@@ -7,26 +7,6 @@
 
 #define TAG "gui"
 
-static lv_obj_t* screen_with_top_bar(lv_obj_t* parent) {
-    lv_obj_set_style_bg_blacken(parent);
-
-    lv_obj_t* vertical_container = lv_obj_create(parent);
-    lv_obj_set_size(vertical_container, LV_PCT(100), LV_PCT(100));
-    lv_obj_set_flex_flow(vertical_container, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_style_no_padding(vertical_container);
-    lv_obj_set_style_bg_blacken(vertical_container);
-
-    top_bar(vertical_container);
-
-    lv_obj_t* child_container = lv_obj_create(vertical_container);
-    lv_obj_set_width(child_container, LV_PCT(100));
-    lv_obj_set_flex_grow(child_container, 1);
-    lv_obj_set_style_no_padding(vertical_container);
-    lv_obj_set_style_bg_blacken(vertical_container);
-
-    return child_container;
-}
-
 static lv_obj_t* create_app_views(lv_obj_t* parent, AppFlags flags) {
     // TODO: Move statusbar into separate ViewPort?
     // TODO: Move toolbar into separate ViewPort?
@@ -44,8 +24,7 @@ static lv_obj_t* create_app_views(lv_obj_t* parent, AppFlags flags) {
     }
 
     if (flags.show_toolbar) {
-        // TODO: store data in GUI
-        // TODO: make some kind of Toolbar struct to hold the title and back icon
+        // TODO: Make some kind of Toolbar struct to hold the title and back icon
         const AppManifest* manifest = loader_get_current_app();
         if (manifest != NULL) {
             toolbar(vertical_container, TOP_BAR_HEIGHT, manifest);
@@ -63,14 +42,6 @@ static lv_obj_t* create_app_views(lv_obj_t* parent, AppFlags flags) {
     return child_container;
 }
 
-static void gui_redraw_app(Gui* gui) {
-    // No need to lock, as this is done by gui_redraw()
-    ViewPort* view_port = gui->app_view_port;
-    furi_assert(view_port);
-    lv_obj_t* container = create_app_views(gui->lvgl_parent, gui->app_flags);
-    view_port_show(view_port, container);
-}
-
 void gui_redraw(Gui* gui) {
     furi_assert(gui);
 
@@ -81,7 +52,10 @@ void gui_redraw(Gui* gui) {
     lv_obj_clean(gui->lvgl_parent);
 
     if (gui->app_view_port != NULL) {
-        gui_redraw_app(gui);
+        ViewPort* view_port = gui->app_view_port;
+        furi_assert(view_port);
+        lv_obj_t* container = create_app_views(gui->lvgl_parent, gui->app_flags);
+        view_port_show(view_port, container);
     } else {
         FURI_LOG_W(TAG, "nothing to draw");
     }
