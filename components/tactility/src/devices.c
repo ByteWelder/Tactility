aa6f05e54c707f6626026c7033e1a385b9153541
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-01-17 23:56:02 +0100

    Use TT logging instead of ESP

--- components/tactility/src/devices.c
+++ components/tactility/src/devices.c
@@ -1,28 +1,26 @@
 #include "check.h"
 #include "devices_i.h"
-#include "esp_check.h"
-#include "esp_err.h"
 
 #define TAG "hardware"
 
 Hardware tt_hardware_init(const HardwareConfig _Nonnull* config) {
     if (config->bootstrap != NULL) {
-        ESP_LOGI(TAG, "Bootstrapping");
+        TT_LOG_I(TAG, "Bootstrapping");
         config->bootstrap();
     }
 
     tt_check(config->display_driver != NULL, "no display driver configured");
     DisplayDriver display_driver = config->display_driver();
-    ESP_LOGI(TAG, "display with driver %s", display_driver.name);
+    TT_LOG_I(TAG, "display with driver %s", display_driver.name);
     DisplayDevice* display = tt_display_device_alloc(&display_driver);
 
     TouchDevice* touch = NULL;
     if (config->touch_driver != NULL) {
         TouchDriver touch_driver = config->touch_driver();
-        ESP_LOGI(TAG, "touch with driver %s", touch_driver.name);
+        TT_LOG_I(TAG, "touch with driver %s", touch_driver.name);
         touch = tt_touch_alloc(&touch_driver);
     } else {
-        ESP_LOGI(TAG, "no touch configured");
+        TT_LOG_I(TAG, "no touch configured");
         touch = NULL;
     }
 
