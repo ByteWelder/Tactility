706b12315ddd6a5849133cc9a79a8f2c5aaf7b83
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-01-20 19:15:21 +0100

    cleanup

--- app-sim/src/freertos.c
+++ app-sim/src/freertos.c
@@ -1,35 +1,13 @@
 #include "tactility.h"
 
 #include "FreeRTOS.h"
-#include "semphr.h"
 #include "task.h"
 
 #define TAG "freertos"
 
-/* Priorities at which the tasks are created.  The event semaphore task is
-given the maximum priority of ( configMAX_PRIORITIES - 1 ) to ensure it runs as
-soon as the semaphore is given. */
 #define mainQUEUE_RECEIVE_TASK_PRIORITY (tskIDLE_PRIORITY + 2)
-#define mainQUEUE_SEND_TASK_PRIORITY (tskIDLE_PRIORITY + 1)
-#define mainEVENT_SEMAPHORE_TASK_PRIORITY (configMAX_PRIORITIES - 1)
-
-/* The rate at which data is sent to the queue, specified in milliseconds, and
-converted to ticks using the pdMS_TO_TICKS() macro. */
-#define mainQUEUE_SEND_PERIOD_MS pdMS_TO_TICKS(200)
-
-/* The period of the example software timer, specified in milliseconds, and
-converted to ticks using the pdMS_TO_TICKS() macro. */
-#define mainSOFTWARE_TIMER_PERIOD_MS pdMS_TO_TICKS(1000)
-
-/* The number of items the queue can hold.  This is 1 as the receive task
-has a higher priority than the send task, so will remove items as they are added,
-meaning the send task should always find the queue empty. */
-#define mainQUEUE_LENGTH (1)
 
 _Noreturn void app_main(void*);
-
-/* The queue used by the queue send and queue receive tasks. */
-static QueueHandle_t xQueue = NULL;
 
 extern void lvgl_init();
 extern void lvgl_task(void*);
