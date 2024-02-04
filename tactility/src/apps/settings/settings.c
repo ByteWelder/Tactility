93e4378a9efe46f7e51cbcbc3dc556c895288d04
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-02-04 17:59:25 +0100

    Various improvements (#34)
    * Cleanup function declarations
    * Align window toolbar title to the left

--- tactility/src/apps/settings/settings.c
+++ tactility/src/apps/settings/settings.c
@@ -11,14 +11,14 @@ static void on_app_pressed(lv_event_t* e) {
     }
 }
 
-static void create_app_widget(const AppManifest* manifest, void* _Nullable parent) {
+static void create_app_widget(const AppManifest* manifest, void* parent) {
     tt_check(parent);
     lv_obj_t* list = (lv_obj_t*)parent;
     lv_obj_t* btn = lv_list_add_btn(list, LV_SYMBOL_FILE, manifest->name);
     lv_obj_add_event_cb(btn, &on_app_pressed, LV_EVENT_CLICKED, (void*)manifest);
 }
 
-static void on_show(TT_UNUSED App app, TT_UNUSED lv_obj_t* parent) {
+static void on_show(TT_UNUSED App app, lv_obj_t* parent) {
     lv_obj_t* list = lv_list_create(parent);
     lv_obj_set_size(list, LV_PCT(100), LV_PCT(100));
     lv_obj_center(list);
