1f66bd92154ebac34f713c019391ac70ae2b732e
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-02-10 22:48:55 +0100

    Implemented orientation setting

--- tactility/src/hardware.c
+++ tactility/src/hardware.c
@@ -1,11 +1,29 @@
 #include "hardware_i.h"
+
+#include "lvgl.h"
+#include "preferences.h"
 #include "sdcard_i.h"
 
 #define TAG "hardware"
 
-typedef struct {
-    bool initialized;
-} Hardware;
+static void init_display_settings(const HardwareConfig* config) {
+    SetBacklightDuty set_backlight_duty = config->display.set_backlight_duty;
+    if (set_backlight_duty != NULL) {
+        int32_t backlight_duty = 200;
+        if (!tt_preferences()->opt_int32("display", "backlight_duty", &backlight_duty)) {
+            tt_preferences()->put_int32("display", "backlight_duty", backlight_duty);
+        }
+        int32_t safe_backlight_duty = TT_MIN(backlight_duty, 255);
+        set_backlight_duty((uint8_t)safe_backlight_duty);
+    }
+
+    int32_t rotation;
+    if (tt_preferences()->opt_int32("display", "rotation", &rotation)) {
+        if (rotation != LV_DISP_ROT_NONE) {
+            lv_disp_set_rotation(lv_disp_get_default(), rotation);
+        }
+    }
+}
 
 void tt_hardware_init(const HardwareConfig* config) {
     if (config->bootstrap != NULL) {
@@ -21,4 +39,6 @@ void tt_hardware_init(const HardwareConfig* config) {
 
     tt_check(config->init_lvgl, "lvlg init not set");
     tt_check(config->init_lvgl(), "lvgl init failed");
+
+    init_display_settings(config);
 }
