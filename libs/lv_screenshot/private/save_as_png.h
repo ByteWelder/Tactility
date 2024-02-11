26ab99aa2cadd79599e22173c2d4166be3833093
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-02-11 21:41:15 +0100

    private headers

--- libs/lv_screenshot/private/save_as_png.h
+++ libs/lv_screenshot/private/save_as_png.h
@@ -7,9 +7,9 @@
 extern "C" {
 #endif
 
-#if LV_USE_PNG == 0
-  #error "LV_USE_PNG must be defined in lv_conf.h"
-#endif
+//#if LV_USE_PNG == 0
+//  #error "LV_USE_PNG must be defined in lv_conf.h"
+//#endif
 
 bool save_as_png_file(const uint8_t* image, uint32_t w, uint32_t h, uint32_t bpp, const char* filename);
 
