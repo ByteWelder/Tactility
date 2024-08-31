6ee27732ba0c06f144a98130fa0ca27f4a2326cc
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-08-31 21:02:50 +0200

    refactored service init
    init headless services from the headless module

--- tactility-headless/src/tactility_headless.h
+++ tactility-headless/src/tactility_headless.h
@@ -1,14 +1,13 @@
 #pragma once
 
 #include "hardware_config.h"
-#include "service_manifest.h"
 #include "tactility_headless_config.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void tt_tactility_headless_init(const HardwareConfig* config, const ServiceManifest* const services[32]);
+void tt_headless_init(const HardwareConfig* config);
 
 const HardwareConfig* tt_get_hardware_config();
 
