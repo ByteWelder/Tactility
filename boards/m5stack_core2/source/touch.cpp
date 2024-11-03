fa48722b1564a39f7e1e21b50bcc99813f46cbfb
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-11-03 22:46:37 +0100

    Introduce touch offset

--- boards/m5stack_core2/source/touch.cpp
+++ boards/m5stack_core2/source/touch.cpp
@@ -8,12 +8,18 @@
 extern "C" {
 #endif
 
+/**
+ * Touch seems to be offset by a certain amount.
+ * The docs don't mention it, so this is the estimated value.
+ */
+#define TOUCH_Y_OFFSET 16
+
 static void read_touch(TT_UNUSED lv_indev_t* indev, lv_indev_data_t* data) {
     lgfx::touch_point_t point; // Making it static makes it unreliable
     bool touched = M5.Lcd.getTouch(&point) > 0;
     if (touched) {
         data->point.x = point.x;
-        data->point.y = point.y;
+        data->point.y = point.y - TOUCH_Y_OFFSET;
         data->state = LV_INDEV_STATE_PRESSED;
     } else {
         data->state = LV_INDEV_STATE_RELEASED;
