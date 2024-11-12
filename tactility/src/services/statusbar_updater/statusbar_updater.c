e3c874786774731e0bfa7d211412dd67974643ba
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-11-12 23:59:39 +0100

    Wi-Fi client improvements and more (#78)
    + Updated docs
    + Wi-Fi mock improved
    + Wi-Fi status icon improved

--- tactility/src/services/statusbar_updater/statusbar_updater.c
+++ tactility/src/services/statusbar_updater/statusbar_updater.c
@@ -26,14 +26,16 @@ typedef struct {
 const char* wifi_get_status_icon_for_rssi(int rssi, bool secured) {
     if (rssi > 0) {
         return TT_ASSETS_ICON_WIFI_CONNECTION_ISSUE;
-    } else if (rssi > -67) {
+    } else if (rssi >= -30) {
         return secured ? TT_ASSETS_ICON_WIFI_SIGNAL_4_LOCKED : TT_ASSETS_ICON_WIFI_SIGNAL_4;
-    } else if (rssi > -70) {
+    } else if (rssi >= -67) {
         return secured ? TT_ASSETS_ICON_WIFI_SIGNAL_3_LOCKED : TT_ASSETS_ICON_WIFI_SIGNAL_3;
-    } else if (rssi > -80) {
+    } else if (rssi >= -70) {
         return secured ? TT_ASSETS_ICON_WIFI_SIGNAL_2_LOCKED : TT_ASSETS_ICON_WIFI_SIGNAL_2;
-    } else {
+    } else if (rssi >= -80) {
         return secured ? TT_ASSETS_ICON_WIFI_SIGNAL_1_LOCKED : TT_ASSETS_ICON_WIFI_SIGNAL_1;
+    } else {
+        return secured ? TT_ASSETS_ICON_WIFI_SIGNAL_0_LOCKED : TT_ASSETS_ICON_WIFI_SIGNAL_0;
     }
 }
 
