8f233164bcd198c8bfd77b2884815cb5548f9471
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-11-18 22:00:13 +0100

    updated ideas and added queue test

--- tests/tactility-core/MessageQueueTest.cpp
+++ tests/tactility-core/MessageQueueTest.cpp
@@ -64,3 +64,21 @@ TEST_CASE("message queue count should decrease when message is consumed") {
     uint32_t count = queue.getCount();
     CHECK_EQ(count, 0);
 }
+
+TEST_CASE("message queue should make copy of data") {
+    // Given a number that we can later delete
+    MessageQueue queue(1, sizeof(int32_t));
+    const int32_t test_value = 123;
+    auto* number = new int32_t();
+    *number = test_value;
+
+    // When we put the number in the queue and then delete it
+    queue.put(number, 100);
+    delete number;
+
+    // We want to verify that the value was copied into the queue and retrieved properly
+    int32_t queue_number = 0;
+    CHECK_EQ(queue.get(&queue_number, 100), TtStatusOk);
+    CHECK_EQ(queue_number, test_value);
+}
+
