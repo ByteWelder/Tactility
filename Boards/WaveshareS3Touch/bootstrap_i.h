e3a73baaef93d8c94ae70b6558dcd07f8eadc6d3
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-11-22 20:07:41 +0100

    fixes and renames

--- Boards/WaveshareS3Touch/bootstrap_i.h
+++ Boards/WaveshareS3Touch/bootstrap_i.h
@@ -1,5 +1,3 @@
 #pragma once
 
-#include <stdbool.h>
-
 bool ws3t_bootstrap();
--- Boards/YellowBoard/yellow_board.h
+++ Boards/YellowBoard/yellow_board.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "hal/Configuration.h"
+#include "Hal/Configuration.h"
 
 // Capacitive touch version of the 2.4" yellow board
 extern const tt::hal::Configuration yellow_board_24inch_cap;
