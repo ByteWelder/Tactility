ed1a66470e9b4a415467da0a586ac68cad9f697a
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-11-24 17:59:01 +0100

    remove test

--- Boards/M5stackCore2/Source/M5stackCore2.cpp
+++ Boards/M5stackCore2/Source/M5stackCore2.cpp
@@ -1,36 +1,8 @@
 #include "M5stackCore2.h"
 #include "M5stackShared.h"
-#include <driver/spi_common.h>
 
-#define TAG "test"
-
-static bool init_spi() {
-    spi_bus_config_t bus_config = {
-        .mosi_io_num = GPIO_NUM_23,
-        .miso_io_num = GPIO_NUM_38,
-        .sclk_io_num = GPIO_NUM_18,
-        .quadwp_io_num = -1, // Quad SPI LCD driver is not yet supported
-        .quadhd_io_num = -1, // Quad SPI LCD driver is not yet supported
-        .max_transfer_sz = 64000,
-    };
-
-    return spi_bus_initialize(SPI2_HOST, &bus_config, SPI_DMA_CH_AUTO) == ESP_OK;
-}
-
-bool init_hardware() {
-
-    TT_LOG_I(TAG, "Init SPI");
-    if (!init_spi()) {
-        TT_LOG_E(TAG, "Init SPI failed");
-        return false;
-    }
-
-    return true;
-
-}
 extern const tt::hal::Configuration m5stack_core2 = {
     .initPower = &m5stack_bootstrap,
-    .initHardware = &init_hardware,
     .initLvgl = &m5stack_lvgl_init,
     .sdcard = &m5stack_sdcard,
     .power = &m5stack_power,
