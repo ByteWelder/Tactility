4ed8fea66e719c65a9765df90026e975c08c7928
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-12-17 20:40:55 +0100

    Deprecate Spacer

--- Tactility/Source/lvgl/Spacer.cpp
+++ Tactility/Source/lvgl/Spacer.cpp
@@ -3,6 +3,7 @@
 
 namespace tt::lvgl {
 
+[[deprecated("Use margin")]]
 lv_obj_t* spacer_create(lv_obj_t* parent, int32_t width, int32_t height) {
     lv_obj_t* spacer = lv_obj_create(parent);
     lv_obj_set_size(spacer, width, height);
