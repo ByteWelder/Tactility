9033daa6ddd50a20a48ef39a275f3aa8693c2e1b
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-12-18 21:39:40 +0100

    Add licenses to TactilitySDK releases (#134)

--- Buildscripts/release-sdk.sh
+++ Buildscripts/release-sdk.sh
@@ -22,9 +22,9 @@ cp build/esp-idf/TactilityC/libTactilityC.a $tactility_library_path/Binary/
 mkdir -p $tactility_library_path/Include
 find_target_dir=$build_dir/$tactility_library_path/Include/
 cd TactilityC/Source
-echo To $find_target_dir
 find -name '*.h' | cpio -pdm $find_target_dir
 cd -
+cp Documentation/license-tactilitysdk.md $build_dir/$tactility_library_path/LICENSE.md
 
 # lvgl
 lvgl_library_path=$library_path/lvgl
@@ -36,12 +36,14 @@ cd Libraries/lvgl
 find src/ -name '*.h' | cpio -pdm $find_target_dir
 cd -
 cp Libraries/lvgl/lvgl.h $find_target_dir
+cp Libraries/lvgl/LICENCE.txt $lvgl_library_path/LICENSE.txt
 cp Libraries/lvgl_conf/lv_conf_kconfig.h $lvgl_library_path/Include/lv_conf.h
 
 # elf_loader
 elf_loader_library_path=$library_path/elf_loader
 mkdir -p $elf_loader_library_path
-cp -r Libraries/elf_loader/elf_loader.cmake $elf_loader_library_path/
+cp Libraries/elf_loader/elf_loader.cmake $elf_loader_library_path/
+cp Libraries/elf_loader/license.txt $elf_loader_library_path/
 
 cp Buildscripts/CMake/TactilitySDK.cmake $target_path/
 cp Buildscripts/CMake/CMakeLists.txt $target_path/
