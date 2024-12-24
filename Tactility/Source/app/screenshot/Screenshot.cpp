6a75d2d3484b0cf168a5e68ce06472025608d495
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2024-12-24 15:57:53 +0000

    ScreenshotUi is now a class

--- Tactility/Source/app/screenshot/Screenshot.cpp
+++ Tactility/Source/app/screenshot/Screenshot.cpp
@@ -9,17 +9,17 @@ namespace tt::app::screenshot {
 
 static void onShow(AppContext& app, lv_obj_t* parent) {
     auto ui = std::static_pointer_cast<ScreenshotUi>(app.getData());
-    create_ui(app, ui, parent);
+    ui->createWidgets(app, parent);
 }
 
 static void onStart(AppContext& app) {
-    auto ui = std::shared_ptr<ScreenshotUi>(new ScreenshotUi());
+    auto ui = std::make_shared<ScreenshotUi>();
     app.setData(ui); // Ensure data gets deleted when no more in use
 }
 
 extern const AppManifest manifest = {
     .id = "Screenshot",
-    .name = "_Screenshot", // So it gets put at the bottom of the desktop and becomes less visible on small screen devices
+    .name = "Screenshot",
     .icon = LV_SYMBOL_IMAGE,
     .type = TypeSystem,
     .onStart = onStart,
--- Tactility/Source/app/screenshot/ScreenshotUi.h
+++ Tactility/Source/app/screenshot/ScreenshotUi.h
@@ -9,15 +9,26 @@
 
 namespace tt::app::screenshot {
 
-typedef struct {
-    lv_obj_t* mode_dropdown;
-    lv_obj_t* path_textarea;
-    lv_obj_t* start_stop_button_label;
-    lv_obj_t* timer_wrapper;
-    lv_obj_t* delay_textarea;
-} ScreenshotUi;
+class ScreenshotUi {
 
-void create_ui(const AppContext& app, std::shared_ptr<ScreenshotUi> ui, lv_obj_t* parent);
+    lv_obj_t* modeDropdown;
+    lv_obj_t* pathTextArea;
+    lv_obj_t* startStopButtonLabel;
+    lv_obj_t* timerWrapper;
+    lv_obj_t* delayTextArea;
+
+    void createTimerSettingsWidgets(lv_obj_t* parent);
+    void createModeSettingWidgets(lv_obj_t* parent);
+    void createFilePathWidgets(lv_obj_t* parent);
+
+    void updateScreenshotMode();
+public:
+
+    void createWidgets(const AppContext& app, lv_obj_t* parent);
+    void onStartPressed();
+    void onModeSet();
+};
+
 
 } // namespace
 
