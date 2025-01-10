333f384f4042e1a2630c8f9c3c3ce5629bd07133
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-01-11 00:29:15 +0100

    Fixes for Windows flash script

--- Buildscripts/Flashing/flash.ps1
+++ Buildscripts/Flashing/flash.ps1
@@ -2,11 +2,10 @@ param(
     $port
 )
 
-if ((Get-Command "esptool.py" -ErrorAction SilentlyContinue) -eq $null)
+if ((Get-Command "esptool" -ErrorAction SilentlyContinue) -eq $null)
 {
-    Write-Host "Unable to find esptool.py in your path. Make sure you have Python installed and on your path. Then run `pip install esptool`."
+    Write-Host "Unable to find esptool in your path. Make sure you have Python installed and on your path. Then run `pip install esptool`."
 }
-
 
 # Create flash command based on partitions
 $json = Get-Content .\Binaries\flasher_args.json -Raw | ConvertFrom-Json
@@ -14,7 +13,7 @@ $jsonClean = $json.flash_files -replace '[\{\}\@\;]', ''
 $jsonClean = $jsonClean -replace '[\=]', ' '
 
 cd Binaries
-$command = "esptool.py --connect-attemps 10 --port $port -b 460800 write_flash $jsonClean"
+$command = "esptool --port $port -b 460800 write_flash $jsonClean"
 Invoke-Expression $command
 cd ..
 
