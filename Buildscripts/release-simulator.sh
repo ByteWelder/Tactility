8b8b20fd8f4f3a62c015014ea7c64811471c794f
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-01-11 15:38:35 +0100

    Implement simulator release for Linux

---
+++ Buildscripts/release-simulator.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+#
+# Usage: release-sdk.sh [target_path]
+# Example: release.sh release/TactilitySDK
+# Description: Releases the current build files as an SDK in the specified folder.
+#
+
+target_path=$1
+
+mkdir -p $target_path
+
+build_dir=`pwd`
+
+cp version.txt $target_path
+
+cmake -S ./ -B buildsim
+cmake --build buildsim --target AppSim
+
+cp buildsim/App/AppSim $target_path/
+cp -r Data/data $target_path/
+cp -r Data/system $target_path/
