5f864af0fa13788a39b4eff69dca3dab4cc3256b
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-01-14 23:25:22 +0100

    Re-added nullability

--- TactilityC/Source/tt_app_alertdialog.cpp
+++ TactilityC/Source/tt_app_alertdialog.cpp
@@ -3,7 +3,7 @@
 
 extern "C" {
 
-void tt_app_alertdialog_start(const char* title, const char* message, const char* buttonLabels[], uint32_t buttonLabelCount) {
+void tt_app_alertdialog_start(const char* title, const char* message, const char* _Nullable buttonLabels[], uint32_t buttonLabelCount) {
     std::vector<std::string> list;
     for (int i = 0; i < buttonLabelCount; i++) {
         const char* item = buttonLabels[i];
--- TactilityC/Source/tt_app_alertdialog.h
+++ TactilityC/Source/tt_app_alertdialog.h
@@ -15,7 +15,7 @@ extern "C" {
  * @param[in] buttonLabels the buttons to show, or null when there are none to show
  * @param[in] buttonLabelCount the amount of buttons (0 or more)
  */
-void tt_app_alertdialog_start(const char* title, const char* message, const char* buttonLabels[], uint32_t buttonLabelCount);
+void tt_app_alertdialog_start(const char* title, const char* message, const char* _Nullable buttonLabels[], uint32_t buttonLabelCount);
 
 /**
  * @return the index of the button that was clicked (the index in the array when start() was called)
