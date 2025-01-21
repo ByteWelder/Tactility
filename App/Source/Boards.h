01e4f5b508a122d34c5bcb96fc28c3fa37f43938
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-01-21 21:41:44 +0100

    Rename CYD board part 1

--- App/Source/Boards.h
+++ App/Source/Boards.h
@@ -7,7 +7,7 @@
 #if defined(CONFIG_TT_BOARD_LILYGO_TDECK)
 #include "LilygoTdeck.h"
 #define TT_BOARD_HARDWARE &lilygo_tdeck
-#elif defined(CONFIG_TT_BOARD_YELLOW_BOARD_24_CAP)
+#elif defined(CONFIG_TT_BOARD_CYD_2432S024C)
 #include "YellowBoard.h"
 #define TT_BOARD_HARDWARE &yellow_board_24inch_cap
 #elif defined(CONFIG_TT_BOARD_M5STACK_CORE2)
