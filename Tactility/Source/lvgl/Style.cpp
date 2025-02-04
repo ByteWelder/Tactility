5ad37ca4ee5d1d3f8923d77f2754cb5a204b4de4
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-02-04 22:14:08 +0100

    Remove unused method and deprecate another

--- Tactility/Source/lvgl/Style.cpp
+++ Tactility/Source/lvgl/Style.cpp
@@ -17,11 +17,4 @@ void obj_set_style_no_padding(lv_obj_t* obj) {
     lv_obj_set_style_pad_gap(obj, LV_STATE_DEFAULT, 0);
 }
 
-void obj_set_style_auto_padding(lv_obj_t* obj) {
-    lv_obj_set_style_pad_top(obj, 8, 0);
-    lv_obj_set_style_pad_bottom(obj, 8, 0);
-    lv_obj_set_style_pad_left(obj, 16, 0);
-    lv_obj_set_style_pad_right(obj, 16, 0);
-}
-
 } // namespace
