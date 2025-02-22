1d36ac3b7e2724a39861c5bfb85fd55b509400a0
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-02-22 22:59:57 +0100

    LVGL improvements for TactilityC

--- Tactility/Include/Tactility/lvgl/Toolbar.h
+++ Tactility/Include/Tactility/lvgl/Toolbar.h
@@ -9,12 +9,45 @@ namespace tt::lvgl {
 #define TOOLBAR_TITLE_FONT_HEIGHT 18
 #define TOOLBAR_ACTION_LIMIT 4
 
-lv_obj_t* toolbar_create(lv_obj_t* parent, const std::string& title);
+/** Create a toolbar widget that shows the app name as title */
 lv_obj_t* toolbar_create(lv_obj_t* parent, const app::AppContext& app);
+
+/** Create a toolbar widget with the provided title*/
+lv_obj_t* toolbar_create(lv_obj_t* parent, const std::string& title);
+
+/** Sets the toolbar title */
 void toolbar_set_title(lv_obj_t* obj, const std::string& title);
-void toolbar_set_nav_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* user_data);
-lv_obj_t* toolbar_add_button_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* user_data);
+
+/** Sets the navigation action of the toolbar (button on the top-left)
+ * @param[in] obj the toolbar instance
+ * @param[in] icon the icon to set on the button
+ * @param[in] callback the callback for the click action of the button
+ * @param[in] callbackEventUserData the user data that is attached to the callback event object
+ */
+void toolbar_set_nav_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* userData);
+
+/**
+ * Create and add an action button to the toolbar (aligned to the right of the toolbar)
+ * @param[in] obj the toolbar instance
+ * @param[in] icon the icon for the action
+ * @param[in] callback the callback for the click action of the button
+ * @param[in] callbackEventUserData the user data that is attached to the callback event object
+ * @return an lv_button instance
+ */
+lv_obj_t* toolbar_add_button_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* userData);
+
+/**
+ * Create and add a switch to the toolbar actions.
+ * @param[in] obj the toolbar instance
+ * @return an instance created by lv_switch_create()
+ */
 lv_obj_t* toolbar_add_switch_action(lv_obj_t* obj);
+
+/**
+ * Create and add a spinner to the toolbar actions.
+ * @param[in] obj the toolbar instance
+ * @return an instance created by Tactility's spinner_create()
+ */
 lv_obj_t* toolbar_add_spinner_action(lv_obj_t* obj);
 
 } // namespace
--- TactilityC/Source/tt_lvgl_toolbar.cpp
+++ TactilityC/Source/tt_lvgl_toolbar.cpp
@@ -3,12 +3,32 @@
 
 extern "C" {
 
-lv_obj_t* tt_lvgl_toolbar_create(lv_obj_t* parent, AppHandle context) {
+lv_obj_t* tt_lvgl_toolbar_create(lv_obj_t* parent, const char* title) {
+    return tt::lvgl::toolbar_create(parent, title);
+}
+
+lv_obj_t* tt_lvgl_toolbar_create_for_app(lv_obj_t* parent, AppHandle context) {
     return tt::lvgl::toolbar_create(parent, *(tt::app::AppContext*)context);
 }
 
-lv_obj_t* tt_lvgl_toolbar_create_simple(lv_obj_t* parent, const char* title) {
-    return tt::lvgl::toolbar_create(parent, title);
+void toolbar_set_title(lv_obj_t* obj, const char* title) {
+    tt::lvgl::toolbar_set_title(obj, title);
+}
+
+void toolbar_set_nav_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* callbackEventUserData) {
+    tt::lvgl::toolbar_set_nav_action(obj, icon, callback, callbackEventUserData);
+}
+
+lv_obj_t* toolbar_add_button_action(lv_obj_t* obj, const char* icon, lv_event_cb_t callback, void* callbackEventUserData) {
+    return tt::lvgl::toolbar_add_button_action(obj, icon, callback, callbackEventUserData);
+}
+
+lv_obj_t* toolbar_add_switch_action(lv_obj_t* obj) {
+    return tt::lvgl::toolbar_add_switch_action(obj);
+}
+
+lv_obj_t* toolbar_add_spinner_action(lv_obj_t* obj) {
+    return tt::lvgl::toolbar_add_spinner_action(obj);
 }
 
 }
