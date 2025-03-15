32be8b59c13a9852a6fd769af0cf693778b9755d
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-03-15 16:47:49 +0100

    Update CYD8048S043C.cpp
    Removed GPS entry

--- Boards/CYD-8048S043C/Source/CYD8048S043C.cpp
+++ Boards/CYD-8048S043C/Source/CYD8048S043C.cpp
@@ -103,13 +103,5 @@ const Configuration cyd_8048s043c_config = {
                 }
             }
         }
-    },
-    .gps = {
-        gps::GpsDevice::Configuration {
-            .name = "Internal",
-            .uartName = "UART1",
-            .baudRate = 9600,
-            .model = gps::GpsModel::ATGM336H
-        }
     }
 };
