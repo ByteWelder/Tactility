f9430ae3d2abff8bf7db2f97d839d88b0f3cca34
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-03-16 21:40:54 +0100

    Fix for names

--- Boards/CYD-2432S024C/Source/CYD2432S024C.h
+++ Boards/CYD-2432S024C/Source/CYD2432S024C.h
@@ -3,4 +3,4 @@
 #include <Tactility/hal/Configuration.h>
 
 // Capacitive touch version of the 2.4" yellow board
-extern const tt::hal::Configuration cyd_2432S024c_config;
+extern const tt::hal::Configuration cyd_2432s024c_config;
