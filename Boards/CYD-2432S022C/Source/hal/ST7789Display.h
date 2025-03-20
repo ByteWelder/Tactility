8dfff8d68412a7080fa3f240c5678483865a4a10
NellowTCS <neeljaiswal23@gmail.com>
2025-03-20 17:11:44 -0600

    Update ST7789Display.h

--- Boards/CYD-2432S022C/Source/hal/ST7789Display.h
+++ Boards/CYD-2432S022C/Source/hal/ST7789Display.h
@@ -2,16 +2,9 @@
 
 #include <Tactility/hal/display/DisplayDevice.h>
 #include <memory>
+#include "CYD2432C022CConstants.h"
 
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
-
-// Constants for the display
-namespace cyd_2432s022c {
-    static constexpr int HORIZONTAL_RESOLUTION = 240;
-    static constexpr int VERTICAL_RESOLUTION = 320;
-    static constexpr int DRAW_BUFFER_HEIGHT = VERTICAL_RESOLUTION / 10;
-    static constexpr int DRAW_BUFFER_SIZE = HORIZONTAL_RESOLUTION * DRAW_BUFFER_HEIGHT;
-}
 
 class ST7789Display : public tt::hal::display::DisplayDevice {
 public:
@@ -35,18 +28,4 @@ private:
     void set_address_window(int x, int y, int w, int h);
 
     std::unique_ptr<Configuration> config_;
-
-    // Pin definitions (from schematic)
-    static constexpr int PIN_D0 = 15;
-    static constexpr int PIN_D1 = 13;
-    static constexpr int PIN_D2 = 12;
-    static constexpr int PIN_D3 = 14;
-    static constexpr int PIN_D4 = 27;
-    static constexpr int PIN_D5 = 25;
-    static constexpr int PIN_D6 = 33;
-    static constexpr int PIN_D7 = 32;
-    static constexpr int PIN_WR = 4;
-    static constexpr int PIN_RD = 2;
-    static constexpr int PIN_RS = 16;
-    static constexpr int PIN_CS = 17;
 };
