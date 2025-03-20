cb478c7120ef9d8fa7e911bd36303c35bb8f6c0e
NellowTCS <neeljaiswal23@gmail.com>
2025-03-20 17:16:31 -0600

    Update CST820Touch.cpp

--- Boards/CYD-2432S022C/Source/hal/CST820Touch.cpp
+++ Boards/CYD-2432S022C/Source/hal/CST820Touch.cpp
@@ -1,7 +1,8 @@
 #include "CST820Touch.h"
+#include "CYD2432S022CConstants.h"
 #include "esp_log.h"
 
-static const char *TAG = "Cst820Touch";
+static const char *TAG = "CST820Touch";
 
 CST820Touch::CST820Touch(std::unique_ptr<Configuration> config)
     : config_(std::move(config)) {}
@@ -10,10 +11,10 @@ bool CST820Touch::read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
     uint8_t touch_data[6];
     i2c_cmd_handle_t cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (I2C_ADDR << 1) | I2C_MASTER_WRITE, true);
+    i2c_master_write_byte(cmd, (CYD_2432S022C_TOUCH_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
     i2c_master_write_byte(cmd, 0x01, true);
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (I2C_ADDR << 1) | I2C_MASTER_READ, true);
+    i2c_master_write_byte(cmd, (CYD_2432S022C_TOUCH_I2C_ADDRESS << 1) | I2C_MASTER_READ, true);
     i2c_master_read(cmd, touch_data, 6, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd);
     esp_err_t err = i2c_master_cmd_begin(config_->i2c_port, cmd, pdMS_TO_TICKS(100));
@@ -39,11 +40,11 @@ bool CST820Touch::read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data) {
     return false; // No more data to read
 }
 
-std::shared_ptr<tt::hal::touch::TouchDevice> createCST820Touch() {
+std::shared_ptr<tt::hal::touch::TouchDevice> create_cst820_touch() {
     auto configuration = std::make_unique<CST820Touch::Configuration>(
-        I2C_NUM_0,
-        cyd_2432s022c::HORIZONTAL_RESOLUTION,
-        cyd_2432s022c::VERTICAL_RESOLUTION
+        CYD_2432S022C_TOUCH_I2C_PORT,
+        CYD_2432S022C_LCD_HORIZONTAL_RESOLUTION,
+        CYD_2432S022C_LCD_VERTICAL_RESOLUTION
     );
 
     return std::make_shared<CST820Touch>(std::move(configuration));
