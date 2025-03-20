469feda9085a926301d61a4ec55483938e147a35
NellowTCS <neeljaiswal23@gmail.com>
2025-03-20 16:05:18 -0600

    Add files via upload

---
+++ Libraries/LovyanGFX/src/LovyanGFX.hpp
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------/
+  Lovyan GFX library - LCD graphics library .
+
+Original Source:
+ https://github.com/lovyan03/LovyanGFX/
+
+Licence:
+ [BSD](https://github.com/lovyan03/LovyanGFX/blob/master/license.txt)
+
+Author:
+ [lovyan03](https://twitter.com/lovyan03)
+
+Contributors:
+ [ciniml](https://github.com/ciniml)
+ [mongonta0716](https://github.com/mongonta0716)
+ [tobozo](https://github.com/tobozo)
+/----------------------------------------------------------------------------*/
+#ifndef LOVYANGFX_HPP_
+#define LOVYANGFX_HPP_
+
+#ifdef setFont
+#undef setFont
+#endif
+
+#include "lgfx/v1_init.hpp"
+
+#if defined ( LGFX_AUTODETECT )
+
+  #include "LGFX_AUTODETECT.hpp"
+
+#endif
+
+
+#endif
