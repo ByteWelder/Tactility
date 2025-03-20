e79e2fda9691b27620e15968f4ee6aadfbc504ce
NellowTCS <neeljaiswal23@gmail.com>
2025-03-20 17:17:30 -0600

    Update YellowSDCard.cpp

--- Boards/CYD-2432S022C/Source/hal/YellowSDCard.cpp
+++ Boards/CYD-2432S022C/Source/hal/YellowSDCard.cpp
@@ -1,19 +1,17 @@
 #include "YellowSDCard.h"
+#include "CYD2432S022CConstants.h"
 #include <Tactility/hal/sdcard/SpiSdCardDevice.h>
-
-static constexpr gpio_num_t SDCARD_PIN_CS = GPIO_NUM_5; // From schematic
-static constexpr spi_host_device_t SDCARD_SPI_HOST = SPI3_HOST;
 
 std::shared_ptr<tt::hal::sdcard::SdCardDevice> createYellowSDCard() {
     auto* configuration = new tt::hal::sdcard::SpiSdCardDevice::Config(
-        SDCARD_PIN_CS,
+        CYD_2432S022C_SDCARD_PIN_CS,
         GPIO_NUM_NC,
         GPIO_NUM_NC,
         GPIO_NUM_NC,
         tt::hal::sdcard::SdCardDevice::MountBehaviour::AtBoot,
         std::make_shared<tt::Mutex>(),
         std::vector<gpio_num_t>(),
-        SDCARD_SPI_HOST
+        CYD_2432S022C_SDCARD_SPI_HOST
     );
 
     auto* sdcard = new tt::hal::sdcard::SpiSdCardDevice(
