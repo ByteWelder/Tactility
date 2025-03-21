ba7c828558749bc5ac6412cedf0f633fa07d2e50
NellowTCS <neeljaiswal23@gmail.com>
2025-03-21 11:22:01 -0600

    Update LovyanDisplay.h

--- Boards/CYD-2432S022C/Source/hal/LovyanDisplay.h
+++ Boards/CYD-2432S022C/Source/hal/LovyanDisplay.h
@@ -1,7 +1,25 @@
 #pragma once
 
 #include <Tactility/hal/display/DisplayDevice.h>
-#include <memory>
 #include "lvgl.h"
+#include <memory>
+
+class LovyanGFXDisplay;  // Forward declaration
 
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
+
+class LovyanGFXDisplay : public tt::hal::display::DisplayDevice {
+public:
+    // Existing methods...
+    bool start() override;
+    bool stop() override;
+    void setBacklightDuty(uint8_t backlightDuty) override;
+    bool supportsBacklightDuty() const override;
+    lv_display_t* getLvglDisplay() const override;
+    std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() override;
+    std::string getName() const override;
+    std::string getDescription() const override;
+
+    // New method to set rotation
+    void setRotation(lv_display_rotation_t rotation);
+};
