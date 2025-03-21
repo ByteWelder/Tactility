998306186196be77de07453f5d940fe118ebbd9f
NellowTCS <neeljaiswal23@gmail.com>
2025-03-21 11:34:00 -0600

    Update LovyanDisplay.h

--- Boards/CYD-2432S022C/Source/hal/LovyanDisplay.h
+++ Boards/CYD-2432S022C/Source/hal/LovyanDisplay.h
@@ -3,14 +3,25 @@
 #include <Tactility/hal/display/DisplayDevice.h>
 #include "lvgl.h"
 #include <memory>
+#include <LovyanGFX.h>
 
-class LovyanGFXDisplay;  // Forward declaration
+class LovyanGFXDisplay;
 
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
 
 class LovyanGFXDisplay : public tt::hal::display::DisplayDevice {
 public:
-    // Existing methods...
+    struct Configuration {
+        uint16_t width;
+        uint16_t height;
+        std::shared_ptr<tt::hal::touch::TouchDevice> touch;
+        Configuration(uint16_t width, uint16_t height, std::shared_ptr<tt::hal::touch::TouchDevice> touch)
+            : width(width), height(height), touch(std::move(touch)) {}
+    };
+
+    explicit LovyanGFXDisplay(std::unique_ptr<Configuration> config);
+    ~LovyanGFXDisplay() override;
+
     bool start() override;
     bool stop() override;
     void setBacklightDuty(uint8_t backlightDuty) override;
@@ -19,7 +30,19 @@ public:
     std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() override;
     std::string getName() const override;
     std::string getDescription() const override;
-
-    // New method to set rotation
     void setRotation(lv_display_rotation_t rotation);
+
+private:
+    std::unique_ptr<Configuration> configuration;
+    class LGFX_CYD_2432S022C : public lgfx::LGFX_Device {
+    public:
+        lgfx::Panel_ST7789 _panel_instance;
+        lgfx::Bus_Parallel8 _bus_instance;
+        lgfx::Light_PWM _light_instance;
+
+        LGFX_CYD_2432S022C();
+    };
+    LGFX_CYD_2432S022C lcd;
+    lv_display_t* lvglDisplay = nullptr;
+    bool isStarted = false;
 };
