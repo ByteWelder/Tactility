a80d085bd5c6c78ab0a64c7bb30ffd8a35e856d2
NellowTCS <neeljaiswal23@gmail.com>
2025-03-21 20:24:42 -0600

    Update CYD2432S022C.cpp

--- Boards/CYD-2432S022C/Source/CYD2432S022C.cpp
+++ Boards/CYD-2432S022C/Source/CYD2432S022C.cpp
@@ -1,14 +1,12 @@
 #include "CYD2432S022C.h"
-#include "hal/YellowDisplay.h"
+#include "hal/LovyanDisplay.h"
 #include "hal/YellowSDCard.h"
 #include "hal/CYD2432S022CConstants.h"
 #include <Tactility/lvgl/LvglSync.h>
 
-// Forward declaration of the display factory function
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
 
 bool init_boot() {
-    // No backlight PWM for this board (not specified in the schematic)
     return true;
 }
 
@@ -18,7 +16,6 @@ const tt::hal::Configuration cyd_2432s022c_config = {
     .sdcard = createYellowSDCard(),
     .power = nullptr,
     .i2c = {
-        // I2C for CST820 touch
         tt::hal::i2c::Configuration {
             .name = "First",
             .port = CYD_2432S022C_TOUCH_I2C_PORT,
@@ -35,28 +32,9 @@ const tt::hal::Configuration cyd_2432s022c_config = {
                 },
                 .clk_flags = 0
             }
-        },
-        // Second I2C port (disabled)
-        tt::hal::i2c::Configuration {
-            .name = "Second",
-            .port = I2C_NUM_1,
-            .initMode = tt::hal::i2c::InitMode::Disabled,
-            .isMutable = true,
-            .config = (i2c_config_t) {
-                .mode = I2C_MODE_MASTER,
-                .sda_io_num = GPIO_NUM_NC,
-                .scl_io_num = GPIO_NUM_NC,
-                .sda_pullup_en = GPIO_PULLUP_DISABLE,
-                .scl_pullup_en = GPIO_PULLUP_DISABLE,
-                .master = {
-                    .clk_speed = 400000
-                },
-                .clk_flags = 0
-            }
         }
     },
     .spi = {
-        // SPI for SD card
         tt::hal::spi::Configuration {
             .device = CYD_2432S022C_SDCARD_SPI_HOST,
             .dma = SPI_DMA_CH_AUTO,
@@ -77,30 +55,6 @@ const tt::hal::Configuration cyd_2432s022c_config = {
                 .intr_flags = 0
             },
             .initMode = tt::hal::spi::InitMode::ByTactility,
-            .isMutable = false,
-            .lock = nullptr
-        },
-        // Second SPI (disabled)
-        tt::hal::spi::Configuration {
-            .device = SPI2_HOST,
-            .dma = SPI_DMA_CH_AUTO,
-            .config = {
-                .mosi_io_num = GPIO_NUM_NC,
-                .miso_io_num = GPIO_NUM_NC,
-                .sclk_io_num = GPIO_NUM_NC,
-                .quadwp_io_num = GPIO_NUM_NC,
-                .quadhd_io_num = GPIO_NUM_NC,
-                .data4_io_num = GPIO_NUM_NC,
-                .data5_io_num = GPIO_NUM_NC,
-                .data6_io_num = GPIO_NUM_NC,
-                .data7_io_num = GPIO_NUM_NC,
-                .data_io_default_level = false,
-                .max_transfer_sz = 8192,
-                .flags = 0,
-                .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
-                .intr_flags = 0
-            },
-            .initMode = tt::hal::spi::InitMode::Disabled,
             .isMutable = false,
             .lock = nullptr
         }
