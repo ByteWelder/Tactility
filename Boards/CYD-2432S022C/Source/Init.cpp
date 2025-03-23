fa3ad847c9a6b90ef20de8691227191599946d6e
NellowTCS <neeljaiswal23@gmail.com>
2025-03-23 16:47:56 -0600

    Update Init.cpp

--- Boards/CYD-2432S022C/Source/Init.cpp
+++ Boards/CYD-2432S022C/Source/Init.cpp
@@ -13,7 +13,7 @@
 #define BUFFER_HEIGHT 64
 
 // Default orientation if not set in NVS
-#define DEFAULT_ORIENTATION LV_DISPLAY_ROTATION_90
+#define DEFAULT_ORIENTATION LV_DISPLAY_ROTATION_0
 
 
 bool cyd22_init() {
