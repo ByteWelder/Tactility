d0bdc6e6909b75aea4e68a00d07d44622da845c4
NellowTCS <neeljaiswal23@gmail.com>
2025-03-23 21:06:57 -0600

    Update YellowSDCard.cpp

--- Boards/CYD-2432S022C/Source/hal/YellowSDCard.cpp
+++ Boards/CYD-2432S022C/Source/hal/YellowSDCard.cpp
@@ -1,10 +1,14 @@
 #include "YellowSDCard.h"
 #include "CYD2432S022CConstants.h"
 #include <Tactility/hal/sdcard/SpiSdCardDevice.h>
+#include <esp_log.h>
+
 
 using tt::hal::sdcard::SpiSdCardDevice;
 
 std::shared_ptr<SdCardDevice> createYellowSDCard() {
+    ESP_LOGI("SDCard", "Heap free before SD card init: %d bytes",
+             heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
     auto* configuration = new SpiSdCardDevice::Config(
         CYD_2432S022C_SDCARD_PIN_CS,
         GPIO_NUM_NC,
@@ -20,5 +24,7 @@ std::shared_ptr<SdCardDevice> createYellowSDCard() {
         std::unique_ptr<SpiSdCardDevice::Config>(configuration)
     );
 
+    ESP_LOGI("SDCard", "Heap free after SD card init: %d bytes",
+             heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
     return std::shared_ptr<SdCardDevice>(sdcard);
 }
