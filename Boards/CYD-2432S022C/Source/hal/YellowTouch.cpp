c6dfd8f5cc1b8994bc8e3ff50dd2389b2843f5c3
NellowTCS <neeljaiswal23@gmail.com>
2025-03-24 15:13:11 -0600

    Update YellowTouch.cpp

--- Boards/CYD-2432S022C/Source/hal/YellowTouch.cpp
+++ Boards/CYD-2432S022C/Source/hal/YellowTouch.cpp
@@ -5,15 +5,16 @@
 
 std::shared_ptr<tt::hal::touch::TouchDevice> createYellowTouch() {
     lv_display_rotation_t rotation = tt::app::display::getRotation();
-    bool mirrorX = (rotation == LV_DISPLAY_ROTATION_90 || rotation == LV_DISPLAY_ROTATION_270);
-    bool mirrorY = (rotation == LV_DISPLAY_ROTATION_90 || rotation == LV_DISPLAY_ROTATION_270);
+    bool swapXY = (rotation == LV_DISPLAY_ROTATION_90 || rotation == LV_DISPLAY_ROTATION_270);
+    bool mirrorX = (rotation == LV_DISPLAY_ROTATION_270); // Mirror X when USB is left
+    bool mirrorY = (rotation == LV_DISPLAY_ROTATION_90);  // Mirror Y when USB is right
     auto configuration = std::make_unique<Cst816sTouch::Configuration>(
-        CYD_2432S022C_TOUCH_I2C_PORT,           // I2C_NUM_0
+        CYD_2432S022C_TOUCH_I2C_PORT,
         CYD_2432S022C_LCD_HORIZONTAL_RESOLUTION, // 240
         CYD_2432S022C_LCD_VERTICAL_RESOLUTION,   // 320
-        false,   // swapXY
-        mirrorX, // Mirror X in landscape
-        mirrorY  // Mirror Y in landscape
+        swapXY,   // Swap X/Y in landscape
+        mirrorX,  // Mirror X as needed
+        mirrorY   // Mirror Y as needed
     );
     return std::make_shared<Cst816sTouch>(std::move(configuration));
 }
