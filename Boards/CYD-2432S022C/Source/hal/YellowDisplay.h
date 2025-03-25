996782f28ebc573a34ef14cf6097edd9aa114379
NellowTCS <neeljaiswal23@gmail.com>
2025-03-25 04:39:27 -0600

    Update YellowDisplay.h

--- Boards/CYD-2432S022C/Source/hal/YellowDisplay.h
+++ Boards/CYD-2432S022C/Source/hal/YellowDisplay.h
@@ -2,5 +2,13 @@
 
 #include <Tactility/hal/display/DisplayDevice.h>
 #include <memory>
+#include "esp_err.h"
+#include "lvgl.h"
 
-std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
+namespace tt::hal::display {
+
+std::shared_ptr<DisplayDevice> createDisplay();
+void setRotation(lv_display_rotation_t rotation);
+esp_lcd_panel_handle_t getLcdPanelHandle();
+
+} // namespace tt::hal::display
