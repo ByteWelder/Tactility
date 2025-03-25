de7af01b49528ae643cd0eaca8426305de482d8c
NellowTCS <neeljaiswal23@gmail.com>
2025-03-25 04:42:28 -0600

    Update YellowDisplay.h

--- Boards/CYD-2432S024C/Source/hal/YellowDisplay.h
+++ Boards/CYD-2432S024C/Source/hal/YellowDisplay.h
@@ -1,6 +1,47 @@
 #pragma once
 
-#include "Tactility/hal/display/DisplayDevice.h"
+#include <Tactility/hal/display/DisplayDevice.h>
 #include <memory>
+#include "esp_err.h"
+#include "lvgl.h"
 
-std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay();
+namespace tt::hal::display {
+
+class YellowDisplay : public DisplayDevice {
+public:
+    struct Configuration {
+        int pclkHz;
+        gpio_num_t csPin;
+        gpio_num_t dcPin;
+        gpio_num_t wrPin;
+        gpio_num_t rstPin;
+        gpio_num_t backlightPin;
+        gpio_num_t dataPins[8];
+        int horizontalResolution;
+        int verticalResolution;
+        std::shared_ptr<tt::hal::touch::TouchDevice> touch;
+
+        // Optional settings
+        bool mirrorX = false;
+        bool mirrorY = false;
+        bool swapXY = false;
+    };
+
+    explicit YellowDisplay(std::unique_ptr<Configuration> config);
+    ~YellowDisplay() override;
+
+    lv_display_t* getLvglDisplay() const override;
+
+    void setRotation(lv_display_rotation_t rotation);
+
+private:
+    std::unique_ptr<Configuration> config;
+    esp_lcd_panel_handle_t panelHandle;
+    lv_display_t* lvglDisplay;
+
+    void initialize();
+};
+
+std::shared_ptr<DisplayDevice> createDisplay();
+
+} // namespace tt::hal::display
