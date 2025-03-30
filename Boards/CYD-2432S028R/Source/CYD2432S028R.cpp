203dd5300990efd6fec7f086fc54e3cb6af66cb9
NellowTCS <neeljaiswal23@gmail.com>
2025-03-30 17:37:06 -0600

    Update CYD2432S028R.cpp

--- Boards/CYD-2432S028R/Source/CYD2432S028R.cpp
+++ Boards/CYD-2432S028R/Source/CYD2432S028R.cpp
@@ -1,6 +1,5 @@
 #include "CYD2432S028R.h"
 #include "hal/YellowDisplay.h"
-#include "hal/YellowSdCard.h"
 #include "hal/YellowDisplayConstants.h"
 #include <Tactility/lvgl/LvglSync.h>
 #include <PwmBacklight.h>
@@ -41,30 +40,6 @@ const tt::hal::Configuration cyd_2432s028r_config = {
             .initMode = tt::hal::spi::InitMode::ByTactility,
             .isMutable = false,
             .lock = tt::lvgl::getSyncLock()
-        },
-        // SPI3_HOST for SD card and touch (shared pins)
-        tt::hal::spi::Configuration {
-            .device = SPI3_HOST,
-            .dma = SPI_DMA_CH_AUTO,
-            .config = {
-                .mosi_io_num = GPIO_NUM_32,
-                .miso_io_num = GPIO_NUM_39,
-                .sclk_io_num = GPIO_NUM_25,
-                .quadwp_io_num = GPIO_NUM_NC,
-                .quadhd_io_num = GPIO_NUM_NC,
-                .data4_io_num = GPIO_NUM_NC,
-                .data5_io_num = GPIO_NUM_NC,
-                .data6_io_num = GPIO_NUM_NC,
-                .data7_io_num = GPIO_NUM_NC,
-                .data_io_default_level = false,
-                .max_transfer_sz = 8192,
-                .flags = 0,
-                .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
-                .intr_flags = 0
-            },
-            .initMode = tt::hal::spi::InitMode::ByTactility,
-            .isMutable = false,
-            .lock = nullptr
         }
     }
 };
