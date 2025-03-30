b95cadc4df99f31f088605c695161db5986883e8
NellowTCS <neeljaiswal23@gmail.com>
2025-03-30 14:34:07 -0600

    Update YellowSDCard.cpp

--- Boards/CYD-2432S028R/Source/hal/YellowSDCard.cpp
+++ Boards/CYD-2432S028R/Source/hal/YellowSDCard.cpp
@@ -1,29 +1,22 @@
 #include "YellowSdCard.h"
-
-#define TAG "twodotfour_sdcard"
-
+#include "YellowDisplayConstants.h"
 #include <Tactility/hal/sdcard/SpiSdCardDevice.h>
-
-#define SDCARD_SPI_HOST SPI3_HOST
-#define SDCARD_PIN_CS GPIO_NUM_15
 
 using tt::hal::sdcard::SpiSdCardDevice;
 
 std::shared_ptr<SdCardDevice> createYellowSdCard() {
     auto* configuration = new SpiSdCardDevice::Config(
-        SDCARD_PIN_CS,
-        GPIO_NUM_NC,
+        CYD_SDCARD_PIN_CS,
+        GPIO_NUM_NC, // No card detect pin specified
         GPIO_NUM_NC,
         GPIO_NUM_NC,
         SdCardDevice::MountBehaviour::AtBoot,
         std::make_shared<tt::Mutex>(),
         std::vector<gpio_num_t>(),
-        SDCARD_SPI_HOST
+        CYD_SDCARD_SPI_HOST
     );
 
-    auto* sdcard = (SdCardDevice*) new SpiSdCardDevice(
-        std::unique_ptr<SpiSdCardDevice::Config>(configuration)
+    return std::shared_ptr<SdCardDevice>(
+        new SpiSdCardDevice(std::unique_ptr<SpiSdCardDevice::Config>(configuration))
     );
-
-    return std::shared_ptr<SdCardDevice>(sdcard);
 }
