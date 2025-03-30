b9335b56117aa7bd64d53c930a7d4c2c8533a280
NellowTCS <neeljaiswal23@gmail.com>
2025-03-30 16:50:35 -0600

    Update YellowDisplay.cpp

--- Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
+++ Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
@@ -5,9 +5,10 @@
 #include <PwmBacklight.h>
 #include "esp_log.h"
 
+static const char* TAG = "XPT2046";
 
 static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() {
-    ESP_LOGI("XPT2046", "Initializing touch with CS=%d, IRQ=%d", CYD_TOUCH_PIN_CS, CYD_TOUCH_PIN_IRQ);
+    ESP_LOGI(TAG, "Initializing touch with CS=%d, IRQ=%d", CYD_TOUCH_PIN_CS, CYD_TOUCH_PIN_IRQ);
     auto configuration = std::make_unique<Xpt2046Touch::Configuration>(
         CYD_TOUCH_SPI_HOST,
         CYD_TOUCH_PIN_CS,
@@ -15,16 +16,8 @@ static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() {
         CYD_DISPLAY_HORIZONTAL_RESOLUTION,
         CYD_DISPLAY_VERTICAL_RESOLUTION
     );
-    configuration->spi_config = {
-        .mosi_io_num = GPIO_NUM_32,
-        .miso_io_num = GPIO_NUM_39,
-        .sclk_io_num = GPIO_NUM_25,
-        .max_transfer_sz = 4096,
-        .spi_mode = SPI_MODE0,
-        .pclk_hz = 2000000
-    };
     auto touch = std::make_shared<Xpt2046Touch>(std::move(configuration));
-    ESP_LOGI("XPT2046", "Touch initialized");
+    ESP_LOGI(TAG, "Touch initialized");
     return touch;
 }
 
