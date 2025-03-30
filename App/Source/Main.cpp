12a0f84708605d38ac96690f434904ec81039c69
NellowTCS <neeljaiswal23@gmail.com>
2025-03-30 12:06:15 -0600

    Update Main.cpp

--- App/Source/Main.cpp
+++ App/Source/Main.cpp
@@ -34,40 +34,4 @@ void app_main() {
     tt::run(config);
 }
 
-} // extern "C"#include "Boards.h"
-#include <Tactility/Tactility.h>
-
-#ifdef ESP_PLATFORM
-#include "tt_init.h"
-#endif
-
-// Forward declaration of clock_app from Clock.cpp
-namespace tt::app::clock {
-    extern const AppManifest clock_app;
-}
-
-extern const tt::app::AppManifest hello_world_app;
-
-extern "C" {
-
-void app_main() {
-    static const tt::Configuration config = {
-        /**
-         * Auto-select a board based on the ./sdkconfig.board.* file
-         * that you copied to ./sdkconfig before you opened this project.
-         */
-        .hardware = TT_BOARD_HARDWARE,
-        .apps = {
-            &hello_world_app,
-            &tt::app::clock::clock_app,
-        }
-    };
-
-#ifdef ESP_PLATFORM
-    tt_init_tactility_c(); // ELF bindings for side-loading on ESP32
-#endif
-
-    tt::run(config);
-}
-
 } // extern "C"
