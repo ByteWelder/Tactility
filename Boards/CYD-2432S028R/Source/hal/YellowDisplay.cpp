7d583c39936caf888d2e6a33a7e7ad7b64bb3970
NellowTCS <neeljaiswal23@gmail.com>
2025-03-29 23:28:45 -0600

    Update YellowDisplay.cpp

--- Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
+++ Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
@@ -9,7 +9,7 @@
 static const char* TAG = "display";
 
 std::shared_ptr<Xpt2046Touch> createTouch() {
-    ESP_LOGI(TAG, "Creating touch");
+    ESP_LOGI(TAG, "Creating touch on SPI1_HOST");
     auto configuration = std::make_unique<Xpt2046Touch::Configuration>(
         SPI1_HOST,
         YELLOW_TOUCH_PIN_CS,
@@ -19,13 +19,15 @@ std::shared_ptr<Xpt2046Touch> createTouch() {
         true,
         false
     );
-    return std::make_shared<Xpt2046Touch>(std::move(configuration));
+    auto touch = std::make_shared<Xpt2046Touch>(std::move(configuration));
+    ESP_LOGI(TAG, "Touch created");
+    return touch;
 }
 
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
     ESP_LOGI(TAG, "Creating touch for display");
     auto touch = createTouch();
-    ESP_LOGI(TAG, "Creating ILI9341 display");
+    ESP_LOGI(TAG, "Creating ILI9341 display on SPI2_HOST");
     auto configuration = std::make_unique<Ili934xDisplay::Configuration>(
         YELLOW_LCD_SPI_HOST,
         YELLOW_LCD_PIN_CS,
