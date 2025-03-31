7123051841d04c702195bd1afa0114983d9070ff
NellowTCS <neeljaiswal23@gmail.com>
2025-03-30 21:53:45 -0600

    Update SoftXpt2046Touch.cpp

--- Boards/CYD-2432S028R/Source/hal/SoftXpt2046Touch.cpp
+++ Boards/CYD-2432S028R/Source/hal/SoftXpt2046Touch.cpp
@@ -1,11 +1,8 @@
-#define LV_USE_PRIVATE_API 1
-
-#include <lvgl.h>
-// #include <lv_hal_indev.h>
 #include "SoftXpt2046Touch.h"
 #include <Tactility/Log.h>
 #include <Tactility/lvgl/LvglSync.h>
 #include "esp_log.h"
+
 #define TAG "soft_xpt2046"
 
 SoftXpt2046Touch::SoftXpt2046Touch(std::unique_ptr<Configuration> config)
@@ -21,17 +18,16 @@ bool SoftXpt2046Touch::start(lv_display_t* display) {
     }
     touch.setRotation(1);  // Match display orientation
 
-    lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = readCallback;
-    indev_drv.user_data = this;
-    indev = lv_indev_drv_register(&indev_drv);
-
+    // LVGL v9: Create input device directly
+    indev = lv_indev_create();
     if (indev == nullptr) {
-        TT_LOG_E(TAG, "Failed to register LVGL input device");
+        TT_LOG_E(TAG, "Failed to create LVGL input device");
         return false;
     }
+
+    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
+    lv_indev_set_read_cb(indev, readCallback);
+    lv_indev_set_user_data(indev, this);
 
     ESP_LOGI(TAG, "Software SPI touch initialized");
     return true;
@@ -46,9 +42,8 @@ bool SoftXpt2046Touch::stop() {
 }
 
 void SoftXpt2046Touch::readCallback(lv_indev_t* indev, lv_indev_data_t* data) {
-    auto* touch = static_cast<SoftXpt2046Touch*>(indev->user_data);
-    uint16_t x, y;
-    uint16_t z;  // Changed to uint16_t to match Z_THRESHOLD (400)
+    auto* touch = static_cast<SoftXpt2046Touch*>(lv_indev_get_user_data(indev));
+    uint16_t x, y, z;
     touch->touch.readData(&x, &y, &z);
 
     if (z >= Z_THRESHOLD) {
@@ -58,8 +53,8 @@ void SoftXpt2046Touch::readCallback(lv_indev_t* indev, lv_indev_data_t* data) {
         if (touch->config->mirrorY) ty = 4095 - ty;
         data->point.x = (tx * touch->config->xMax) / 4095;
         data->point.y = (ty * touch->config->yMax) / 4095;
-        data->state = LV_INDEV_STATE_PR;
+        data->state = LV_INDEV_STATE_PRESSED;
     } else {
-        data->state = LV_INDEV_STATE_REL;
+        data->state = LV_INDEV_STATE_RELEASED;
     }
 }
