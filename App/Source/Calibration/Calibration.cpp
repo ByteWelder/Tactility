49a43e9cc3b6df77a50a1d622c836afa29fd3d34
NellowTCS <neeljaiswal23@gmail.com>
2025-04-01 10:34:32 -0600

    Update Calibration.cpp

--- App/Source/Calibration/Calibration.cpp
+++ App/Source/Calibration/Calibration.cpp
@@ -80,11 +80,12 @@ namespace tt::app {
             ESP_LOGI("Calibration", "Step %d: rawX=%d, rawY=%d", step, rawX, rawY);
         }
     }
+};
 
-    // Define the manifest
-    const AppManifest calibration_app = {
-        .id = "Calibration",
-        .name = "Touch Calibration",
-        .createApp = create<Calibration>
-    };
-}
+// Define the manifest
+extern const AppManifest calibration_app = {
+    .id = "Calibration",
+    .name = "Touch Calibration",
+    .createApp = create<Calibration>
+};
+
