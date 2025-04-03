b3b53f5bd513e50dce72447cac7d0e83a93c7a21
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-04-03 23:23:47 +0200

    Add comment

--- ExternalApps/HelloWorld/build.sh
+++ ExternalApps/HelloWorld/build.sh
@@ -1,6 +1,6 @@
 rm sdkconfig
 cp ../../sdkconfig sdkconfig
 cat sdkconfig.override >> sdkconfig
+# First we must run "build" because otherwise "idf.py elf" is not a valid command
 idf.py build
 idf.py elf
-
