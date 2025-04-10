d29167279322ad9afb02b02fd7de7fc11db96159
NellowTCS <neeljaiswal23@gmail.com>
2025-04-09 20:05:14 -0600

    Update CYD2432S028R.cpp

--- Boards/CYD-2432S028R/Source/CYD2432S028R.cpp
+++ Boards/CYD-2432S028R/Source/CYD2432S028R.cpp
@@ -40,6 +40,30 @@ const tt::hal::Configuration cyd_2432s028r_config = {
             .initMode = tt::hal::spi::InitMode::ByTactility,
             .isMutable = false,
             .lock = tt::lvgl::getSyncLock()
-        }
+        },
+        tt::hal::spi::Configuration {
+            .device = SPI3_HOST,
+            .dma = SPI_DMA_CH_AUTO,
+            .config = {
+                .mosi_io_num = GPIO_NUM_23,
+                .miso_io_num = GPIO_NUM_19,
+                .sclk_io_num = GPIO_NUM_18,
+                .quadwp_io_num = GPIO_NUM_NC,
+                .quadhd_io_num = GPIO_NUM_NC,
+                .data4_io_num = GPIO_NUM_NC,
+                .data5_io_num = GPIO_NUM_NC,
+                .data6_io_num = GPIO_NUM_NC,
+                .data7_io_num = GPIO_NUM_NC,
+                .data_io_default_level = false,
+                .max_transfer_sz = CYD_SPI_TRANSFER_SIZE_LIMIT,
+                .flags = 0,
+                .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
+                .intr_flags = 0
+            },
+            .initMode = tt::hal::spi::InitMode::ByTactility,
+            .isMutable = false,
+            .lock = tt::lvgl::getSyncLock()
+        },
+
     }
 };
