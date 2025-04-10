f31e11b6dc9be36454f5c9e11e52f625fe525f2b
NellowTCS <neeljaiswal23@gmail.com>
2025-04-10 15:41:40 -0600

    Update YellowDisplay.cpp

--- Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
+++ Boards/CYD-2432S028R/Source/hal/YellowDisplay.cpp
@@ -12,13 +12,13 @@ static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() {
     auto config = std::make_unique<SoftXpt2046Touch::Configuration>(
         CYD_DISPLAY_HORIZONTAL_RESOLUTION,  // xMax = 240
         CYD_DISPLAY_VERTICAL_RESOLUTION,    // yMax = 320
-        false,   // swapXy (raw X → mapped Y, raw Y → mapped X)
+        true,   // swapXy (raw X → mapped Y, raw Y → mapped X)
         false,  // mirrorX
         true,   // mirrorY
-        231,    // xMinRaw (Bottom-Right raw Y)
-        276,    // xMaxRaw (Top-Left raw Y)
-        91,     // yMinRaw (Bottom-Right raw X)
-        168     // yMaxRaw (Top-Left raw X)
+        163,    // xMinRaw (Bottom-Left raw Y)
+        269,    // xMaxRaw (Bottom-Right raw Y)
+        74,     // yMinRaw (Bottom-Left raw X)
+        188     // yMaxRaw (Top-Left raw X)
     );
     return std::make_shared<SoftXpt2046Touch>(std::move(config));
 }
