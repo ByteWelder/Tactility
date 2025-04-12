8d05e1e8e86468555500c5199a1c784b5c0254ec
NellowTCS <neeljaiswal23@gmail.com>
2025-04-12 14:37:02 -0600

    Update XPT2046_TouchscreenSOFTSPI.h

--- Drivers/XPT2046-SoftSPI/XPT2046_TouchscreenSOFTSPI.h
+++ Drivers/XPT2046-SoftSPI/XPT2046_TouchscreenSOFTSPI.h
@@ -1,53 +1,33 @@
 #pragma once
 
-#include "driver/gpio.h"
-#include "esp_attr.h"
-#include "SoftSPI.h"
-#include <stdint.h>
-#include "../../Boards/CYD-2432S028R/Source/hal/YellowDisplayConstants.h"
+#include "esp_lcd_touch_xpt2046.h"
 
-#define SPI_SETTING 0  // Mode 0 (CPOL=0, CPHA=0)
-
-class TS_Point {
+class XPT2046_SoftSPI_Wrapper {
 public:
-    TS_Point() : x(0), y(0), z(0) {}
-    TS_Point(int16_t x, int16_t y, int16_t z) : x(x), y(y), z(z) {}
-    bool operator==(TS_Point p) { return ((p.x == x) && (p.y == y) && (p.z == z)); }
-    bool operator!=(TS_Point p) { return ((p.x != x) || (p.y != y) || (p.z != z)); }
-    int16_t x, y, z;
-};
+    struct Config {
+        gpio_num_t cs_pin;
+        gpio_num_t int_pin;
+        gpio_num_t miso_pin;
+        gpio_num_t mosi_pin;
+        gpio_num_t sck_pin;
+        uint16_t x_max;
+        uint16_t y_max;
+        bool swap_xy;
+        bool mirror_x;
+        bool mirror_y;
+        uint16_t x_min_raw;
+        uint16_t x_max_raw;
+        uint16_t y_min_raw;
+        uint16_t y_max_raw;
+    };
 
-struct CalibrationData {
-    float xScale, xOffset;
-    float yScale, yOffset;
-    bool valid;
-};
+    static std::unique_ptr<XPT2046_SoftSPI_Wrapper> create(const Config& config);
+    ~XPT2046_SoftSPI_Wrapper() = default;
 
-template<gpio_num_t MisoPin, gpio_num_t MosiPin, gpio_num_t SckPin, uint8_t Mode = SPI_SETTING>
-class XPT2046_TouchscreenSOFTSPI {
-public:
-    XPT2046_TouchscreenSOFTSPI(gpio_num_t csPin, gpio_num_t tirqPin = GPIO_NUM_NC);
-    bool begin();
-    TS_Point getPoint();
-    bool tirqTouched();
-    bool touched();
-    void readData(uint16_t* x, uint16_t* y, uint16_t* z);
-    void setRotation(uint8_t n) { rotation = n % 4; }
-    void getRawTouch(uint16_t& rawX, uint16_t& rawY);
-    CalibrationData getCalibration();
-    void setCalibration(const CalibrationData& cal);
+    esp_lcd_touch_handle_t get_touch_handle() const { return driver_->get_handle(); }
+    lv_indev_t* get_lvgl_indev() const { return driver_->get_lvgl_indev(); }
 
 private:
-    IRAM_ATTR static void isrPin(void* arg);
-    void update();
-    uint16_t readXOY(uint8_t cmd);
-    gpio_num_t csPin, tirqPin;
-    volatile bool isrWake = false;
-    uint8_t rotation = 0;
-    int16_t xraw = 0, yraw = 0, zraw = 0;
-    uint32_t msraw = 0x80000000;
-    SoftSPI<MisoPin, MosiPin, SckPin, Mode> touchscreenSPI;
-    CalibrationData calData = {1.0f, 0.0f, 1.0f, 0.0f, false};
+    XPT2046_SoftSPI_Wrapper(std::unique_ptr<XPT2046_SoftSPI> driver) : driver_(std::move(driver)) {}
+    std::unique_ptr<XPT2046_SoftSPI> driver_;
 };
-
-extern XPT2046_TouchscreenSOFTSPI<CYD_TOUCH_MISO_PIN, CYD_TOUCH_MOSI_PIN, CYD_TOUCH_SCK_PIN, 0> touch;
