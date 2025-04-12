25bc5e93f7e2b4dcca3865e506237d45f2aff3e6
NellowTCS <neeljaiswal23@gmail.com>
2025-04-12 15:15:14 -0600

    Update XPT2046-SoftSPI.cpp

--- Drivers/XPT2046-SoftSPI/XPT2046-SoftSPI.cpp
+++ Drivers/XPT2046-SoftSPI/XPT2046-SoftSPI.cpp
@@ -4,26 +4,31 @@
 static const char* TAG = "XPT2046_Wrapper";
 
 std::unique_ptr<XPT2046_SoftSPI_Wrapper> XPT2046_SoftSPI_Wrapper::create(const Config& config) {
+    esp_lcd_touch_xpt2046_config_t touch_config = {
+        .base = {
+            .int_gpio_num = config.int_pin,
+            .x_max = config.x_max,
+            .y_max = config.y_max,
+            .interrupt_callback = nullptr,
+            .levels = {.interrupt_level = false},
+            .user_data = nullptr
+        },
+        .x_min_raw = config.x_min_raw,
+        .x_max_raw = config.x_max_raw,
+        .y_min_raw = config.y_min_raw,
+        .y_max_raw = config.y_max_raw,
+        .swap_xy = config.swap_xy,
+        .mirror_x = config.mirror_x,
+        .mirror_y = config.mirror_y
+    };
+
     XPT2046_SoftSPI::Config driver_config = {
         .cs_pin = config.cs_pin,
         .int_pin = config.int_pin,
         .miso_pin = config.miso_pin,
         .mosi_pin = config.mosi_pin,
         .sck_pin = config.sck_pin,
-        .touch_config = {
-            .int_gpio_num = config.int_pin,
-            .x_max = config.x_max,
-            .y_max = config.y_max,
-            .swap_xy = config.swap_xy,
-            .mirror_x = config.mirror_x,
-            .mirror_y = config.mirror_y,
-            .x_min_raw = config.x_min_raw,
-            .x_max_raw = config.x_max_raw,
-            .y_min_raw = config.y_min_raw,
-            .y_max_raw = config.y_max_raw,
-            .interrupt_callback = nullptr,
-            .levels = {.interrupt_level = false}
-        }
+        .touch_config = touch_config
     };
 
     auto driver = XPT2046_SoftSPI::create(driver_config);
@@ -32,6 +37,8 @@ std::unique_ptr<XPT2046_SoftSPI_Wrapper> XPT2046_SoftSPI_Wrapper::create(const C
         return nullptr;
     }
 
+    driver->get_handle()->config.user_data = &driver_config.touch_config;
+
     ESP_LOGI(TAG, "XPT2046 wrapper created");
-    return std::make_unique<XPT2046_SoftSPI_Wrapper>(std::move(driver));
+    return std::unique_ptr<XPT2046_SoftSPI_Wrapper>(new XPT2046_SoftSPI_Wrapper(std::move(driver)));
 }
