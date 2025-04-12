bb19e2bc2c89797c4b2afc6a9c1dfe502ddf218c
NellowTCS <neeljaiswal23@gmail.com>
2025-04-12 14:52:14 -0600

    Update SoftSPI.h

--- Drivers/XPT2046-SoftSPI/SoftSPI.h
+++ Drivers/XPT2046-SoftSPI/SoftSPI.h
@@ -7,12 +7,11 @@ public:
     SoftSPI(gpio_num_t miso, gpio_num_t mosi, gpio_num_t sck);
     void begin();
     uint8_t transfer(uint8_t data);
-    void cs_low() { gpio_set_level(cs_pin_, 0); }
-    void cs_high() { gpio_set_level(cs_pin_, 1); }
+    void cs_low();
+    void cs_high();
 
 private:
     gpio_num_t miso_;
     gpio_num_t mosi_;
     gpio_num_t sck_;
-    gpio_num_t cs_pin_;
 };
