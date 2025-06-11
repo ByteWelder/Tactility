c26a0f5c4fe6afaf8347a4d7d4e76bfe3b2b8cab
NellowTCS <neeljaiswal23@gmail.com>
2025-06-11 09:11:29 -0600

    Update Tactiligotchi.h

--- App/Source/Tactiligotchi/Tactiligotchi.h
+++ App/Source/Tactiligotchi/Tactiligotchi.h
@@ -29,7 +29,7 @@ public:
     ~TamagotchiApp() override = default;
 
     void onShow(tt::app::AppContext& context, lv_obj_t* parent) override;
-    void onHide() override;
+    virtual void onHide(AppContext& appContext);
 
     // Must be public so PatternGame can call them:
     void gameSuccess();
