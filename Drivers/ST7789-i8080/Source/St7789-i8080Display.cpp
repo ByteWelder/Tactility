77a0827331f146eb3bb46a65e5c9b5b327c623ce
NellowTCS <neeljaiswal23@gmail.com>
2025-07-19 19:31:01 -0600

    Update St7789-i8080Display.cpp

--- Drivers/ST7789-i8080/Source/St7789-i8080Display.cpp
+++ Drivers/ST7789-i8080/Source/St7789-i8080Display.cpp
@@ -57,11 +57,11 @@ bool St7789I8080Display::start() {
             .swap_color_bytes = 0,
             .pclk_idle_low = 0,
             .pclk_active_neg = 0
-        }
+        },
         .on_color_trans_done = nullptr,
         .user_ctx = nullptr,
         .lcd_cmd_bits = 8,
-        .lcd_param_bits = 8,
+        .lcd_param_bits = 8
     };
 
     if (esp_lcd_new_panel_io_i80(i80Bus, &io_config, &ioHandle) != ESP_OK) {
