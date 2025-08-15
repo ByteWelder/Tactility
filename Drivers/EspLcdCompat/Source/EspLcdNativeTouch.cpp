0279568c6857379f274083fb67b9f8450c1f07df
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-08-15 18:54:20 +0200

    Fix for native touch

--- Drivers/EspLcdCompat/Source/EspLcdNativeTouch.cpp
+++ Drivers/EspLcdCompat/Source/EspLcdNativeTouch.cpp
@@ -1,5 +1,13 @@
 #include "EspLcdNativeTouch.h"
 
-bool EspLcdNativeTouch::getTouchedPoints(uint16_t* x, uint16_t* y, uint16_t* strength, uint8_t* pointCount, uint8_t maxPointCount) {
+#include <Tactility/LogEsp.h>
+
+constexpr char* TAG = "EspLcdNativeTouch";
+
+bool EspLcdNativeTouch::getTouchedPoints(uint16_t* x, uint16_t* y, uint16_t* _Nullable strength, uint8_t* pointCount, uint8_t maxPointCount) {
+    if (esp_lcd_touch_read_data(handle) != ESP_OK) {
+        TT_LOG_E(TAG, "Read data failed");
+        return false;
+    }
     return esp_lcd_touch_get_coordinates(handle, x, y, strength, pointCount, maxPointCount) == ESP_OK;
 }
