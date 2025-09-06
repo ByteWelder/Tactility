a0c818b4caa034dc4ba61a6cc520c852acad8718
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-09-06 23:01:55 +0200

    Made DisplayDevice more robust

--- Boards/Simulator/Source/hal/SdlDisplay.h
+++ Boards/Simulator/Source/hal/SdlDisplay.h
@@ -13,19 +13,18 @@ public:
     std::string getName() const override { return "SDL Display"; }
     std::string getDescription() const override { return ""; }
 
-    bool start() override {
-        return displayHandle != nullptr;
-    }
-
+    bool start() override { return true; }
     bool stop() override { tt_crash("Not supported"); }
 
     bool supportsLvgl() const override { return true; }
-    bool startLvgl() override { return true; }
+    bool startLvgl() override { return displayHandle != nullptr; }
     bool stopLvgl() override { tt_crash("Not supported"); }
+    lv_display_t* _Nullable getLvglDisplay() const override { return displayHandle; }
 
     std::shared_ptr<tt::hal::touch::TouchDevice> _Nullable getTouchDevice() override { return std::make_shared<SdlTouch>(); }
 
-    lv_display_t* _Nullable getLvglDisplay() const override { return displayHandle; }
+    bool supportsDisplayDriver() const override { return false; }
+    std::shared_ptr<tt::hal::display::DisplayDriver> _Nullable getDisplayDriver() override { return nullptr; }
 };
 
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
