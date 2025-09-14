ac2ff77e375a7733412a529a99048e8c72be9360
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-09-14 17:01:09 +0200

    Remove deprecated HAL config fields

--- Tactility/Include/Tactility/hal/Configuration.h
+++ Tactility/Include/Tactility/hal/Configuration.h
@@ -9,14 +9,6 @@ namespace tt::hal {
 
 typedef bool (*InitBoot)();
 
-namespace display { class DisplayDevice; }
-namespace keyboard { class KeyboardDevice; }
-namespace power { class PowerDevice; }
-
-typedef std::shared_ptr<display::DisplayDevice> (*CreateDisplay)();
-typedef std::shared_ptr<keyboard::KeyboardDevice> (*CreateKeyboard)();
-typedef std::shared_ptr<power::PowerDevice> (*CreatePower)();
-
 typedef std::vector<std::shared_ptr<Device>> DeviceVector;
 
 typedef std::shared_ptr<Device> (*CreateDevice)();
@@ -35,22 +27,6 @@ struct Configuration {
 
     /** Init behaviour: default (esp_lvgl_port for ESP32, nothing for PC) or None (nothing on any platform). Only used in Tactility, not in TactilityHeadless. */
     const LvglInit lvglInit = LvglInit::Default;
-
-    /** Display HAL functionality. */
-    [[deprecated("use createDevices")]]
-    const CreateDisplay _Nullable createDisplay = nullptr;
-
-    /** Keyboard HAL functionality. */
-    [[deprecated("use createDevices")]]
-    const CreateKeyboard _Nullable createKeyboard = nullptr;
-
-    /** An optional SD card interface. */
-    [[deprecated("use createDevices")]]
-    const std::shared_ptr<sdcard::SdCardDevice> _Nullable sdcard = nullptr;
-
-    /** An optional power interface for battery or other power delivery. */
-    [[deprecated("use createDevices")]]
-    const CreatePower _Nullable power = nullptr;
 
     std::function<DeviceVector()> createDevices = [] { return std::vector<std::shared_ptr<Device>>(); };
 
