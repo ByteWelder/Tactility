2b5ad39231b6eaea38bed6aaac3cb25f1f668b45
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2025-10-01 22:53:10 +0200

    TactilityC improvements

--- TactilityC/Source/symbols/stl.cpp
+++ TactilityC/Source/symbols/stl.cpp
@@ -1,7 +1,6 @@
 #include <symbols/stl.h>
 
 #include <bits/functexcept.h>
-#include <memory_resource>
 
 extern "C" {
     extern void* _Znwj(uint32_t size); // operator new(unsigned int)
@@ -14,6 +13,6 @@ const esp_elfsym stl_symbols[] = {
     // Note: You have to use the mangled names here
     { "_ZSt20__throw_length_errorPKc", (void*)&(std::__throw_length_error) },
     { "_ZSt28__throw_bad_array_new_lengthv", (void*)&(std::__throw_bad_array_new_length) },
-    { "_ZNSt3pmr20get_default_resourceEv", (void*)&(std::pmr::get_default_resource) },
+    { "_ZSt17__throw_bad_allocv", (void*)&(std::__throw_bad_alloc) }
     // { "", (void*)&(std::) },
 };
