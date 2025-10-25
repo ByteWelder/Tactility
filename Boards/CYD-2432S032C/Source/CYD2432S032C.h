579bfae67a30ef4bf11282cd82a1a7e864774bcd
NellowTCS <neeljaiswal23@gmail.com>
2025-10-25 16:31:05 -0600

    Why is that one capital S

--- Boards/CYD-2432S032C/Source/CYD2432S032C.h
+++ Boards/CYD-2432S032C/Source/CYD2432S032C.h
@@ -3,4 +3,4 @@
 #include <Tactility/hal/Configuration.h>
 
 // Capacitive touch version of the 2.4" yellow board
-extern const tt::hal::Configuration cyd_2432S032c_config;
+extern const tt::hal::Configuration cyd_2432s032c_config;
