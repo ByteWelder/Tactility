da92563f9aad2784ded7a70c8c36d586197e2e57
NellowTCS <neeljaiswal23@gmail.com>
2025-10-26 12:43:24 -0600

    Manual sync

--- Buildscripts/runtests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cmake -S ./ -B build-sim
-cmake --build build-sim --target build-tests -j 14
-build-sim/Tests/TactilityCore/TactilityCoreTests --exit
-build-sim/Tests/Tactility/TactilityTests --exit
-
