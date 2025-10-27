b24d79f627b5cd77e69754180b01c6b3fe977c3b
NellowTCS <neeljaiswal23@gmail.com>
2025-10-27 11:30:09 -0600

    Add review things

--- Boards/CYD-E32R32P/Source/Configuration.cpp
+++ Boards/CYD-E32R32P/Source/Configuration.cpp
@@ -5,12 +5,10 @@
 #include <Tactility/lvgl/LvglSync.h>
 #include <PwmBacklight.h>
 
-#define CYD_SPI_TRANSFER_SIZE_LIMIT (240 * 320 / 4 * 2)
-
 using namespace tt::hal;
 
 static bool initBoot() {
-    return driver::pwmbacklight::init(CYD_BACKLIGHT_PIN);
+    return driver::pwmbacklight::init(DISPLAY_BACKLIGHT_PIN);
 }
 
 static tt::hal::DeviceVector createDevices() {
@@ -23,15 +21,34 @@ static tt::hal::DeviceVector createDevices() {
 extern const Configuration hardwareConfiguration = {
     .initBoot = initBoot,
     .createDevices = createDevices,
-    .i2c = {},
+    .i2c = {
+        tt::hal::i2c::Configuration {
+            .name = "External",
+            .port = I2C_NUM_0,
+            .initMode = tt::hal::i2c::InitMode::ByTactility,
+            .isMutable = true,
+            .config = (i2c_config_t) {
+                .mode = I2C_MODE_MASTER,
+                .sda_io_num = GPIO_NUM_32,
+                .scl_io_num = GPIO_NUM_25,
+                .sda_pullup_en = false,
+                .scl_pullup_en = false,
+                .master = {
+                    .clk_speed = 400000
+                },
+                .clk_flags = 0
+            }
+        },
+    },
+    // Display
     .spi = {
         tt::hal::spi::Configuration {
             .device = SPI2_HOST,
             .dma = SPI_DMA_CH_AUTO,
             .config = {
-                .mosi_io_num = GPIO_NUM_13, // shared display & touch MOSI
-                .miso_io_num = GPIO_NUM_12, // shared display & touch MISO
-                .sclk_io_num = GPIO_NUM_14, // shared clock
+                .mosi_io_num = GPIO_NUM_13,
+                .miso_io_num = GPIO_NUM_12,
+                .sclk_io_num = GPIO_NUM_14,
                 .quadwp_io_num = GPIO_NUM_NC,
                 .quadhd_io_num = GPIO_NUM_NC,
                 .data4_io_num = GPIO_NUM_NC,
@@ -48,13 +65,14 @@ extern const Configuration hardwareConfiguration = {
             .isMutable = false,
             .lock = tt::lvgl::getSyncLock()
         },
+        // SD Card
         tt::hal::spi::Configuration {
             .device = SPI3_HOST,
             .dma = SPI_DMA_CH_AUTO,
             .config = {
-                .mosi_io_num = GPIO_NUM_23, // SD card MOSI
-                .miso_io_num = GPIO_NUM_19, // SD card MISO
-                .sclk_io_num = GPIO_NUM_18, // SD card SCK
+                .mosi_io_num = GPIO_NUM_23,
+                .miso_io_num = GPIO_NUM_19,
+                .sclk_io_num = GPIO_NUM_18,
                 .quadwp_io_num = GPIO_NUM_NC,
                 .quadhd_io_num = GPIO_NUM_NC,
                 .data4_io_num = GPIO_NUM_NC,
@@ -62,7 +80,7 @@ extern const Configuration hardwareConfiguration = {
                 .data6_io_num = GPIO_NUM_NC,
                 .data7_io_num = GPIO_NUM_NC,
                 .data_io_default_level = false,
-                .max_transfer_sz = CYD_SPI_TRANSFER_SIZE_LIMIT,
+                .max_transfer_sz = 8192,
                 .flags = 0,
                 .isr_cpu_id = ESP_INTR_CPU_AFFINITY_AUTO,
                 .intr_flags = 0
--- Boards/CYD-E32R32P/Source/devices/Display.cpp
+++ Boards/CYD-E32R32P/Source/devices/Display.cpp
@@ -8,10 +8,10 @@
 // Create the XPT2046 touch device (hardware/esp_lcd driver)
 static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() {
     auto config = std::make_unique<Xpt2046Touch::Configuration>(
-        CYD_DISPLAY_SPI_HOST,                                // spi device / bus (SPI2_HOST)
-        CYD_TOUCH_CS_PIN,                                    // touch CS (IO33)
-        (uint16_t)CYD_DISPLAY_HORIZONTAL_RESOLUTION,         // x max
-        (uint16_t)CYD_DISPLAY_VERTICAL_RESOLUTION,           // y max
+        DISPLAY_SPI_HOST,                                // spi device / bus (SPI2_HOST)
+        TOUCH_CS_PIN,                                    // touch CS (IO33)
+        (uint16_t)DISPLAY_HORIZONTAL_RESOLUTION,         // x max
+        (uint16_t)DISPLAY_VERTICAL_RESOLUTION,           // y max
         false,                                               // swapXy
         true,                                                // mirrorX
         true                                                 // mirrorY
@@ -23,15 +23,15 @@ static std::shared_ptr<tt::hal::touch::TouchDevice> createTouch() {
 std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
     // Create the ST7789 panel configuration
     St7789Display::Configuration panel_configuration = {
-        .horizontalResolution = CYD_DISPLAY_HORIZONTAL_RESOLUTION,
-        .verticalResolution = CYD_DISPLAY_VERTICAL_RESOLUTION,
+        .horizontalResolution = DISPLAY_HORIZONTAL_RESOLUTION,
+        .verticalResolution = DISPLAY_VERTICAL_RESOLUTION,
         .gapX = 0,
         .gapY = 0,
         .swapXY = false,
         .mirrorX = false,
         .mirrorY = false,
         .invertColor = false,
-        .bufferSize = 0,  // 0 -> default 1/10 screen
+        .bufferSize = DISPLAY_DRAW_BUFFER_SIZE,  // 0 -> default 1/10 screen
         .touch = createTouch(),
         .backlightDutyFunction = driver::pwmbacklight::setBacklightDuty,
         .resetPin = GPIO_NUM_NC,
@@ -40,9 +40,9 @@ std::shared_ptr<tt::hal::display::DisplayDevice> createDisplay() {
 
     // Create the SPI configuration (from EspLcdSpiDisplay base class)
     auto spi_configuration = std::make_shared<EspLcdSpiDisplay::SpiConfiguration>(EspLcdSpiDisplay::SpiConfiguration {
-        .spiHostDevice = CYD_DISPLAY_SPI_HOST,
-        .csPin = CYD_DISPLAY_PIN_CS,
-        .dcPin = CYD_DISPLAY_PIN_DC,
+        .spiHostDevice = DISPLAY_SPI_HOST,
+        .csPin = DISPLAY_PIN_CS,
+        .dcPin = DISPLAY_PIN_DC,
         .pixelClockFrequency = 40'000'000,
         .transactionQueueDepth = 10
     });
