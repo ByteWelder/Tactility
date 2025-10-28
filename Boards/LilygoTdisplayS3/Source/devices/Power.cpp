ef13cf8ecf28c6fba5ba60418f655e6c8a05df67
NellowTCS <neeljaiswal23@gmail.com>
2025-10-27 22:02:36 -0600

    Fix review things

--- Boards/LilygoTdisplayS3/Source/devices/Power.cpp
+++ Boards/LilygoTdisplayS3/Source/devices/Power.cpp
@@ -1,7 +1,9 @@
+#include "Power.h"
+
 #include <ChargeFromAdcVoltage.h>
 #include <EstimatedPower.h>
 
-std::shared_ptr<PowerDevice> createPower() {
+std::shared_ptr<tt::hal::power::PowerDevice> createPower() {
     ChargeFromAdcVoltage::Configuration configuration;
     // 2.0 ratio, but +.11 added as display voltage sag compensation.
     configuration.adcMultiplier = 2.11;
