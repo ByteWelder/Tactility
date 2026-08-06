7a5afed0bd4628f61cffff5ce9c8cdffc6ada7cc
NellowTCS <neeljaiswal23@gmail.com>
2026-08-06 04:28:56 -0600

    it boots!

---
+++ Devices/cl-32/source/module.cpp
@@ -0,0 +1,9 @@
+#include <tactility/module.h>
+
+extern "C" {
+
+struct Module cl_32_module = {
+    .name = "cl-32"
+};
+
+}
--- Drivers/SSD1685/README.md
+++
@@ -1,3 +0,0 @@
-# SSD1685
-
-A very custom ESP32 LVGL driver for SSD1685 **epaper** displays.
