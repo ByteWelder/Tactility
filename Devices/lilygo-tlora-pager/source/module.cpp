fe31889f8065f7fd85b59b59d8141dca1f643a44
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-08-23 11:22:35 +0200

    Remove unused code

--- Devices/lilygo-tlora-pager/source/module.cpp
+++ Devices/lilygo-tlora-pager/source/module.cpp
@@ -2,8 +2,6 @@
 #include <tactility/module.h>
 #include <tactility/system_event.h>
 
-#include <lilygo/drivers/tpager_encoder_input.h>
-
 constexpr auto* TAG = "T-Lora Pager";
 
 extern "C" {
@@ -12,7 +10,6 @@ static void on_boot_completed(struct SystemEvent* /*event*/, void* /*context*/) 
     // The kernel tpager_encoder device is already started by kernel_init(); this just
     // registers it as an LVGL input device, which requires LVGL to be up first.
     lvgl_lock();
-    tpager_encoder::init();
     lvgl_unlock();
 }
 
