6ff70aedceed4faaca82bf839d4b46ec6833dfcf
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-08-29 16:05:12 +0200

    Remove TactilityC

--- Firmware/Source/Main.cpp
+++ Firmware/Source/Main.cpp
@@ -2,20 +2,13 @@
 
 #include <devicetree.h>
 
-#ifdef ESP_PLATFORM
-#include <tt_init.h>
-#else
+#ifndef ESP_PLATFORM
 #include <Simulator.h>
 #endif
 
 extern "C" {
 
 void app_main() {
-
-#ifdef ESP_PLATFORM
-    tt_init_tactility_c(); // ELF bindings for side-loading on ESP32
-#endif
-
     tt::run(dts_modules, dts_devices);
 }
 
