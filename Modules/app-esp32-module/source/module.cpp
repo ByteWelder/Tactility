4b9124ed146814aea3b7556c70bfe7e068f92010
Ken Van Hoeylandt <git@kenvanhoeylandt.net>
2026-08-29 13:33:02 +0200

    Move symbol mapping

--- Modules/app-esp32-module/source/module.cpp
+++ Modules/app-esp32-module/source/module.cpp
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: Apache-2.0
 #include <app_esp32/module.h>
 
+#include <private/elf_symbol.h>
+
 #include <service/manager.h>
 
 #include <tactility/error.h>
@@ -10,7 +12,18 @@ extern "C" {
 
 extern ServiceManifest loader_service_manifest;
 
+// Overrides elf_loader's default KConfig-based symbol resolver with one that looks up symbols
+// across every started kernel module's own symbol table instead.
+uintptr_t app_esp32_symbol_resolver(const char* symbolName) {
+    uintptr_t symbol_address;
+    if (module_resolve_symbol_global(symbolName, &symbol_address)) {
+        return symbol_address;
+    }
+    return 0;
+}
+
 static error_t start() {
+    elf_set_symbol_resolver(app_esp32_symbol_resolver);
     return service_manager_add(&loader_service_manifest, /*auto_start=*/true);
 }
 
--- TactilityC/Source/tt_init.cpp
+++ TactilityC/Source/tt_init.cpp
@@ -1,83 +1,8 @@
 #ifdef ESP_PLATFORM
 
-#include "private/elf_symbol.h"
-#include "tt_app_alertdialog.h"
-#include "tt_app_fileselection.h"
-#include "tt_app_selectiondialog.h"
-
-#include <cassert>
-#include <cstring>
-#include <ctype.h>
-#include <driver/ledc.h>
-#include <esp_heap_caps.h>
-#include <miniz.h>
-#include <sys/errno.h>
-#include <vector>
-
-#include <Tactility/Tactility.h>
-
-#include <driver/i2s_common.h>
-#include <driver/i2s_std.h>
-#include <driver/gpio.h>
-
-#ifdef CONFIG_IDF_TARGET_ESP32P4
-#include <driver/ppa.h>
-#include <esp_cache.h>
-#endif
-
 extern "C" {
 
-const esp_elfsym main_symbols[] {
-#ifdef __HAVE_LOCALE_INFO__
-    // ctype.h
-    ESP_ELFSYM_EXPORT(__locale_ctype_ptr),
-#else
-    ESP_ELFSYM_EXPORT(_ctype_),
-#endif
-
-    // Tactility
-    ESP_ELFSYM_EXPORT(tt_app_fileselection_start_for_existing_file),
-    ESP_ELFSYM_EXPORT(tt_app_fileselection_start_for_existing_or_new_file),
-    ESP_ELFSYM_EXPORT(tt_app_fileselection_get_result_path),
-    ESP_ELFSYM_EXPORT(tt_app_selectiondialog_start),
-    ESP_ELFSYM_EXPORT(tt_app_alertdialog_start),
-    // delimiter
-    ESP_ELFSYM_END,
-};
-
-uintptr_t resolve_symbol(const esp_elfsym* source, const char* symbolName) {
-    const esp_elfsym* symbol_iterator = source;
-    while (symbol_iterator->name != nullptr) {
-        if (strcmp(symbol_iterator->name, symbolName) == 0) {
-            return reinterpret_cast<uintptr_t>(symbol_iterator->sym);
-        }
-        symbol_iterator++;
-    }
-    return 0;
-}
-
-uintptr_t tt_symbol_resolver(const char* symbolName) {
-    static const std::vector all_symbols = {
-        main_symbols,
-    };
-
-    for (const auto* symbols : all_symbols) {
-        const uintptr_t address = resolve_symbol(symbols, symbolName);
-        if (address != 0) {
-            return address;
-        }
-    }
-
-    uintptr_t symbol_address;
-    if (module_resolve_symbol_global(symbolName, &symbol_address)) {
-        return symbol_address;
-    }
-
-    return 0;
-}
-
 void tt_init_tactility_c() {
-    elf_set_symbol_resolver(tt_symbol_resolver);
 }
 
 }
